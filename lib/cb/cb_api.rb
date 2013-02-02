175879dcb3da59539a5b3129d337a27bb29c4acc
Jesse Retchko <JesseRetchko@gmail.com>
2013-02-02 20:58:51 +0100

    second commit -- still building out the basics

--- lib/cb/cb_api.rb
+++ lib/cb/cb_api.rb
@@ -4,7 +4,7 @@ module Cb
 	class CbApi
 	    include HTTParty
 			base_uri "http://api.careerbuilder.com"
-			default_params :developerkey => Cb.configuration.dev_key.to_s 
+			#default_params :developerkey => Cb.configuration.dev_key
 			#			    :outputjson => InternalApiGem::configuration.use_json.to_s
 			#default_timeout InternalApiGem::configuration.api_time_out
 	end
--- lib/cb/config.rb
+++ lib/cb/config.rb
@@ -20,9 +20,9 @@ module Cb
     private
 
     def set_defaults
-      @cb_api_dev_key   = "WDhd88S735S3V2NWZKPD" # "ruby-cb-api"  # Get a developer key at http://api.careerbuilder.com/RequestDevKey.aspx
-      @api_time_out     = 5
-      @use_json         = true
+      @dev_key      = "WDHH6P96RQD9FSDCZ0G7" # "ruby-cb-api"  # Get a developer key at http://api.careerbuilder.com/RequestDevKey.aspx
+      @time_out     = 5
+      @use_json     = true
     end
   end
 end
