3bb18c64499a879ec4c425a8d4f01995de979e6b
Jesse Retchko <JesseRetchko@gmail.com>
2013-02-03 16:33:11 +0100

    we have VCR and mockweb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,22 @@ end
 require 'cb'
 
 Cb.configure do |config|
-	config.use_json = true			 		 ############################################################
-	config.dev_key = "WDhd88S735S3V2NWZKPD"  #"WDHH6P96RQD9FSDCZ0G7" # CB Ruby API key for unit tests
-	config.time_out = 5					 	 # Register for your own key at http://api.careerbuilder.com/
+	config.use_json = true			 		  ############################################################
+	config.dev_key  = "WDhd88S735S3V2NWZKPD"  #"WDHH6P96RQD9FSDCZ0G7" # CB Ruby API key for unit tests
+	config.time_out = 5					 	  # Register for your own key at http://api.careerbuilder.com/
+end
+
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |c|
+  c.cassette_library_dir     = 'spec/cassettes'
+  c.hook_into                :webmock
+  c.default_cassette_options = { :record => :new_episodes }
+  c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+  #c.extend VCR::RSpec::Macros
+  c.treat_symbols_as_metadata_keys_with_true_values = true
 end
