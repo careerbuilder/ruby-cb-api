e0b7c3aa14fbb97cda3ba4b4c2688303047e6595
Jesse Retchko <JesseRetchko@gmail.com>
2013-02-03 16:33:11 +0100

    we have VCR and mockweb

--- spec/cb/cb_job_api_spec.rb
+++ spec/cb/cb_job_api_spec.rb
@@ -3,9 +3,14 @@ require 'spec_helper'
 module Cb
   describe Cb::CbJobApi do
     context ".search" do
-      it "should perform a blank search" do
-        search = Cb::CbJobApi.search()
-      end
+        it "should perform a blank search", :vcr => {:cassette_name => "search/blank"} do
+            search = Cb::CbJobApi.search()
+
+            search.count.should == 25
+            search[0].is_a?(Cb::CbJob).should == true
+            search[24].is_a?(Cb::CbJob).should == true
+
+        end
     end
   end
 end
