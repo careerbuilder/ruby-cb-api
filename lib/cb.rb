6b7540c1188f7f6a86c4f6d5e60f4e1e49f4e9d0
Jesse Retchko <JesseRetchko@gmail.com>
2013-02-13 00:34:15 -0500

    Job search now takes in a job search criteria object. This object also supports method chaining, for (hopefully) easier usage.

--- lib/cb.rb
+++ lib/cb.rb
@@ -4,6 +4,8 @@ require "cb/cb_job_api"
 require "cb/cb_company"
 require "cb/cb_company_api"
 require "cb/config"
+require "fluid_attributes"
+require "cb/job_search_criteria"
 
 module Cb
 	def self.configure
