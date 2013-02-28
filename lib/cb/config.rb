449609ce8ebc276d13c50cf1fc89e904d635f60c
Uma Maharajan <uma.maharajan@careerbuilder.com>
2013-02-28 11:22:03 -0500

    getting the latest

--- lib/cb/config.rb
+++ lib/cb/config.rb
@@ -2,7 +2,7 @@ module Cb
   class Config
     attr_accessor :dev_key, :time_out, :use_json, 
                   :uri_job_search, :uri_job_find,
-                  :uri_company_find
+                  :uri_company_find, :uri_education_code
 
     def initialize
       set_defaults
@@ -13,16 +13,18 @@ module Cb
       @uri_job_find         ||= "/v1/Job"
 
       @uri_company_find     ||= "/Employer/CompanyDetails"
+      @uri_education_code   ||= "/v1/educationcodes"
     end
 
     def to_hash
       {
-        :dev_key            => @dev_key,
-        :time_out  	        => @time_out,
-        :use_json           => @use_json,
-        :uri_job_search     => @uri_job_search,
-        :uri_job_find       => @uri_job_find,
-        :uri_company_find   => @uri_company_find
+        :dev_key              => @dev_key,
+        :time_out  	          => @time_out,
+        :use_json             => @use_json,
+        :uri_job_search       => @uri_job_search,
+        :uri_job_find         => @uri_job_find,
+        :uri_company_find     => @uri_company_find,
+        :uri_education_code  =>  @uri_education_code
       }
     end
 
@@ -30,7 +32,7 @@ module Cb
     #################################################################
 
     def set_defaults
-      @dev_key              = "ruby-cb-api"  # Get a developer key at http://api.careerbuilder.com
+      @dev_key              = "WDHF7PC72784BJ0NFBZC"  # Get a developer key at http://api.careerbuilder.com
       @time_out             = 5
       @use_json             = true
 
