fbca5dd04f9d9d5c77b19b1e701e39ae1cf5871d
Uma Maharajan <uma.maharajan@careerbuilder.com>
2013-03-15 13:43:17 -0400

    pushing education codes call

--- lib/cb/cb_education_code.rb
+++ lib/cb/cb_education_code.rb
@@ -1,7 +1,7 @@
 module Cb
 	class CbEducationCode
 		extend FluidAttributes
-		fluid_attr_accessor :education_code, :education_name
+		fluid_attr_accessor :education_code, :education_name, :education_language
 
 
     ##############################################################
@@ -12,6 +12,7 @@ module Cb
 		def initialize(args = {})
 
 			@education_code                         = args["Code"] || ""
+			@education_language											=	args["Name"]["@language"] || ""
       @education_name                         = args["Name"]["#text"] || ""
 
     end
