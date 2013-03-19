2234fb7f8d577af0b8eef7c30df1add8b62fd385
Jesse Retchko <JesseRetchko@gmail.com>
2013-03-18 20:35:00 -0400

    Lots and lots of clean up.

--- lib/cb/cb_education_code.rb
+++ lib/cb/cb_education_code.rb
@@ -1,22 +1,17 @@
 module Cb
-	class CbEducationCode
-		extend FluidAttributes
-		fluid_attr_accessor :education_code, :education_name, :education_language
-
+  class CbEducationCode
+    attr_accessor :education_code, :education_name, :education_language
 
     ##############################################################
-		## This object stores Education Codes having to do with a 
+    ## This object stores Education Codes having to do with a
     ## country. The API objects dealing with job, will populate
     ## one or many of this object.
-		##############################################################
-		def initialize(args = {})
+    ##############################################################
+    def initialize(args = {})
 
-			@education_code                         = args["Code"] || ""
-			@education_language											=	args["Name"]["@language"] || ""
-      @education_name                         = args["Name"]["#text"] || ""
-
+      @education_code          = args['Code'] || ''
+      @education_language			 = args['Name']['@language'] || ''
+      @education_name          = args['Name']['#text'] || ''
     end
-
-
   end
 end
