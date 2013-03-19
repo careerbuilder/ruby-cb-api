a3ca6f10865155fdc11b2ec87cb46fb129551dd7
Jesse Retchko <JesseRetchko@gmail.com>
2013-03-18 20:35:00 -0400

    Lots and lots of clean up.

--- spec/cb/cb_education_code_api_spec.rb
+++ spec/cb/cb_education_code_api_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 module Cb
 		describe Cb::EducationCodeApi do 
 
-				context "get_education_code"
-						it "should get default education code if country is blank ", :vcr => { :cassette_name => "educationcode/country"} do				
+				context 'get_education_code'
+						it 'should get default education code if country is blank ', :vcr => { :cassette_name => 'educationcode/country'} do				
 							country_code = ''									
 							ret_codes_by_country = Cb::EducationCodeApi.new().get_education_codes(country_code)
 
 						end
 
-						it "should get the education code for given country ", :vcr => { :cassette_name => "educationcode/country"} do						
+						it 'should get the education code for given country ', :vcr => { :cassette_name => 'educationcode/country'} do						
 							country_code = 'WH'
 							ret_codes_by_country = Cb::EducationCodeApi.new().get_education_codes(country_code)
 	
