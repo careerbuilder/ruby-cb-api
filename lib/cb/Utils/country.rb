fcfb1eeae2b0e4175ed68d9af510b581fb55a1ec
Jesse Retchko <JesseRetchko@gmail.com>
2013-03-18 21:26:49 -0400

    Got country cleaned up

--- lib/cb/Utils/country.rb
+++ lib/cb/Utils/country.rb
@@ -1,19 +1,22 @@
 module Cb::Utils::Country
+  # These aren't all country codes. We get it. Our naming scheme didn't hold up over time.
   def self.get_supported
-    %w(AH,BE,CA,CC,CE,CH,CN,CP,CS,CY,DE,DK,E1,ER,ES,EU,F1,
-       FR,GC,GR,I1,IE,IN,IT,J1,JC,JS,LJ,M1,MY,NL,NO,PD,PI,
-       PL,RM,RO,RX,S1,SE,SF,SG,T1,T2,UK,US,WH,WM,WR)
+    %w(AH BE CA CC CE CH CN CP CS CY DE DK E1 ER ES EU F1 
+       FR GC GR I1 IE IN IT J1 JC JS LJ M1 MY NL NO PD PI 
+       PL RM RO RX S1 SE SF SG T1 T2 UK US WH WM WR)
   end
 
-	def self.is_valid?(code)
-		get_supported.include? code
-	end
+  def self.is_valid?(country)
+    get_supported.include? country
+  end
 
-	def self.inject_convenience_methods
-		get_supported.each do | code |
-			#self.define_method(#{code}) do | throw_away |
-			#	#code
-			#end
-		end
-	end
+  def self.inject_convenience_methods
+    get_supported.each do | country |
+      unless self.respond_to? "#{country}"
+        self.define_singleton_method "#{country}" do
+          return country
+        end
+      end
+    end
+  end
 end
