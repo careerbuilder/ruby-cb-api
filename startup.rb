0925c909c9c09895e806eeb2796bb8c6785b5da7
Chris Little <=>
2013-04-09 14:52:36 -0400

    Reinstall gem in startup

--- startup.rb
+++ startup.rb
@@ -1,6 +1,8 @@
 ## This file is loaded by irb -r
 ## So, setup any necessary configuration variables / executables here
 
+`rake install`
+
 require 'Cb'
 
 ## more configuration options to follow...
