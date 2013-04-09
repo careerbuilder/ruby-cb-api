0cf68fbd55a664f369b2e7a0dcdff5cc41ffae41
Chris Little <=>
2013-04-09 15:48:41 -0400

    Use gem devkey in startup

--- startup.rb
+++ startup.rb
@@ -1,8 +1,10 @@
 ## This file is loaded by irb -r
 ## So, setup any necessary configuration variables / executables here
 
+## Install local source code as gem
 `rake install`
 
 require 'Cb'
+Cb.configuration.dev_key = "WDHH6P96RQD9FSDCZ0G7"
 
 ## more configuration options to follow...
