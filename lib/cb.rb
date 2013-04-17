55134cf8d617de15b2d0da7ab6d7a2e0d6aa8c22
Jesse Retchko <JesseRetchko@gmail.com>
2013-04-17 17:04:29 -0400

    Clean up. Getting ready for first publish to ruby gems.

--- lib/cb.rb
+++ lib/cb.rb
@@ -1,21 +1,3 @@
-# require 'cb/utils/api'
-# require 'cb/utils/meta_values'
-# require 'cb/utils/country'
-# require 'cb/utils/fluid_attributes'
-# require 'cb/cb_job'
-# require 'cb/job_api'
-# require 'cb/recommendation_api'
-# require 'cb/cb_company'
-# require 'cb/company_api'
-# require 'cb/cb_category'
-# require 'cb/category_api'
-# require 'cb/cb_employee_type'
-# require 'cb/employee_types_api'
-# require 'cb/cb_education'
-# require 'cb/education_api'
-# require 'cb/job_application_api'
-# require 'cb/cb_job_application'
-
 require 'cb/config'
 Dir[File.dirname(__FILE__) + '/cb/utils/*.rb'].each {| file| require file }
 Dir[File.dirname(__FILE__) + '/cb/clients/*.rb'].each {| file| require file }
