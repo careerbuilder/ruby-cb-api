d3c31f1b480e5176e661a72c48739d55691a685c
Chris Little <=>
2013-04-18 17:53:38 -0400

    Assume ALL word inflections will be uppercased

--- lib/cb/Utils/string.rb
+++ lib/cb/Utils/string.rb
@@ -1,12 +1,8 @@
 class String
 	
 	# Modified from Rails source: activesupport/lib/active_support/inflector/methods.rb
-  def camelize(uppercase_first_letter = true)
-    if uppercase_first_letter
-      self.sub!(/^[a-z\d]*/) { $&.capitalize }
-    else
-      self.sub!(/^(?:#{inflections.acronym_regex}(?=\b|[A-Z_])|\w)/) { $&.downcase }
-    end
+  def camelize()
+    self.sub!(/^[a-z\d]*/) { $&.capitalize }
     self.gsub(/(?:_|(\/))([a-z\d]*)/) { "#{$2.capitalize}" }.gsub('/', '::')
   end
 end
