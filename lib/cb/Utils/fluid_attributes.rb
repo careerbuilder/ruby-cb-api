e7f33935a59577021328d4c1f3272480a71e0dd9
Chris Little <=>
2013-04-18 18:02:37 -0400

    Add 'attr_writer'-like capability

--- lib/cb/Utils/fluid_attributes.rb
+++ lib/cb/Utils/fluid_attributes.rb
@@ -1,7 +1,10 @@
 module Cb::Utils::FluidAttributes
+
 	def fluid_attr_accessor(*names)
 		names.each do |name|
-			define_method :"#{name}" do | *args |
+
+			puts "Iterating through names: #{name}, about to define"
+			define_method :"#{name}" do | *args |				
 				return instance_variable_get(:"@#{name}") if args.length == 0
 
 				if args.length == 1
@@ -10,6 +13,12 @@ module Cb::Utils::FluidAttributes
 				end
 
 				raise ArgumentError.new("Wrong number of arguments (#{args.length} for 1)")
+			end
+
+			define_method :"#{name}=" do |*args|
+				instance_variable_set(:"@#{name}", args[0]) if args.length == 1
+				return self
+				raise ArgumentError, "Wrong number of arguments (#{args.length} for 1)"
 			end
 		end
 	end
