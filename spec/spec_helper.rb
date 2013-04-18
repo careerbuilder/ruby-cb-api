099f271318af86dc75f87b4e86246adfccc70a6b
Chris Little <=>
2013-04-18 12:52:56 -0400

    Swap demo developer key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ require 'cb'
 
 Cb.configure do |config|
 	config.use_json 	= true                    ########################################################
-	config.dev_key  	= 'WDhd88S735S3V2NWZKPD'  #"WDHH6P96RQD9FSDCZ0G7" # CB Ruby API key for unit tests
+	config.dev_key  	= 'WDHH6P96RQD9FSDCZ0G7'  #"WDHH6P96RQD9FSDCZ0G7" # CB Ruby API key for unit tests
 	config.time_out 	= 5					 	            # Register for your own key at http://api.careerbuilder.com/
 end
 
