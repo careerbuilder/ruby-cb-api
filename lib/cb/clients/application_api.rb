520ffd71a73f81dce56512ad660ecf3da29fb897
Jesse Retchko <JesseRetchko@gmail.com>
2013-05-08 13:52:01 -0400

    Cleaning up applicaiton logic. Still more work to do.

--- lib/cb/clients/application_api.rb
+++ lib/cb/clients/application_api.rb
@@ -1,22 +1,23 @@
 require 'json'
-require 'nokogiri'
+#require 'nokogiri'
 
 module Cb
 	class ApplicationApi
-	#############################################################
+	  #############################################################
     ## Get an application for a job
     ##
     ## For detailed information around this API please visit:
     ## http://api.careerbuilder.com/ApplicationInfo.aspx
     #############################################################
-        def self.for_job(did)
-        	my_api = Cb::Utils::Api.new()
-        	cb_response = my_api.cb_get(Cb.configuration.uri_application, :query => {:JobDID => did})
-        	json_hash = JSON.parse(cb_response.response.body)
-        	app_info = CbApp.new(json_hash['ResponseBlankApplication']['BlankApplication']['Questions'])
-            my_api.append_api_responses(app_info, json_hash['ResponseBlankApplication'])
-            return {:json_hash => json_hash, :app_info => app_info}    
-        end
+    def self.for_job(did)
+      my_api = Cb::Utils::Api.new()
+      cb_response = my_api.cb_get(Cb.configuration.uri_application, :query => {:JobDID => did})
+      json_hash = JSON.parse(cb_response.response.body)
+      app_info = CbApp.new(json_hash['ResponseBlankApplication']['BlankApplication']['Questions'])
+      my_api.append_api_responses(app_info, json_hash['ResponseBlankApplication'])
+
+      return app_info
+    end
 
     #############################################################
     ## Submit a job application for a registered user
@@ -24,24 +25,22 @@ module Cb
     ## For detailed information around this API please visit:
     ## http://api.careerbuilder.com/ApplicationInfo.aspx
     #############################################################
+    def self.submit_registered_app(request_xml)
+      my_api = Cb::Utils::Api.new()
+      cb_response = my_api.cb_post(Cb.configuration.uri_application_registered, :body => request_xml)
 
-        def self.submit_registered_app(request_xml)
-            my_api = Cb::Utils::Api.new()
-            cb_response = my_api.cb_post(Cb.configuration.uri_application_registered, :body => request_xml)
-            
-            json_hash = JSON.parse(cb_response.response.body)
-            status = false
-            begin 
-                json_hash = JSON.parse(cb_response.response.body)
-                if json_hash.empty? == false
-                    status = json_hash['ResponseApplication']['ApplicationStatus'] == 'Complete (Test)'
-                end  
-            rescue
-                status = false
-            end 
-            
-            return status
+      json_hash = JSON.parse(cb_response.response.body)
+      begin
+        json_hash = JSON.parse(cb_response.response.body)
+        if json_hash.empty? == false
+          status = json_hash['ResponseApplication']['ApplicationStatus'] == 'Complete (Test)'
         end
+      rescue
+          status = false
+      end
+
+      return status
+    end
 
     #############################################################
     ## Submit a job application
@@ -49,13 +48,11 @@ module Cb
     ## For detailed information around this API please visit:
     ## http://api.careerbuilder.com/ApplicationInfo.aspx
     #############################################################
-
-        def self.submit_app(request_xml)
-            my_api = Cb::Utils::Api.new()
-            cb_response = my_api.cb_post(Cb.configuration.uri_application_submit, :body => request_xml)
-            json_hash = cb_response.parsed_response
-            status = json_hash['ResponseApplication']['ApplicationStatus'] == 'Complete (Test)'
-           
-        end
+    def self.submit_app(request_xml)
+      my_api = Cb::Utils::Api.new()
+      cb_response = my_api.cb_post(Cb.configuration.uri_application_submit, :body => request_xml)
+      json_hash = cb_response.parsed_response
+      status = json_hash['ResponseApplication']['ApplicationStatus'] == 'Complete (Test)'
     end
+  end
 end
--- spec/cb/cb_application_api_spec.rb
+++ spec/cb/cb_application_api_spec.rb
@@ -2,37 +2,32 @@ require 'spec_helper'
 
 
 module Cb
-    describe Cb::ApplicationApi do
-        context '.for_job' do
-            it 'should return info from application', :vcr => {:cassette_name => 'job/application/for_job'} do
-                #search = Cb.job_search_criteria.location('Atlanta, GA').radius(10).search()
-                search = Cb.job_search_criteria.location('Atlanta, GA').radius(10).keywords('customcodes:CBDEV_applyurlno').search()
-                job = search[Random.new.rand(0..24)]
-               
-                results = Cb.application.for_job job.did
+  describe Cb::ApplicationApi do
+    context '.for_job' do
+      it 'should return info from application', :vcr => {:cassette_name => 'job/application/for_job'} do
+          search = Cb.job_search_criteria.location('Atlanta, GA').radius(10).keywords('customcodes:CBDEV_applyurlno').search()
+          job = search[Random.new.rand(0..24)]
 
-                app_info = results[:app_info]
-                json_hash = results[:json_hash]
-                json_hash['Errors'].nil?.should == true
-                @did = job.did
+          result = Cb.application.for_job job.did
+          result.is_a?(Cb::CbApp).should == true
 
-                job.did.length.should >= 19
-                app_info.is_a?(Cb::CbApp).should == true
-            end
-        end
-
-        context '.submit_app' do
-            it 'should send application info to api', :vcr => {:cassette_name => 'job/application/submit_app'} do
-                request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID>#{Cb.configuration.job_did}</JobDID><HostSite>WR</HostSite><SiteID>cbnsv</SiteID><Responses><Response><QuestionID>ApplicantName</QuestionID><ResponseText>captain morgain</ResponseText></Response><Response><QuestionID>ApplicantEmail</QuestionID><ResponseText>captainmorgan@careerbuilder.com</ResponseText></Response><Response><QuestionID>Resume</QuestionID><ResponseText>Resume goes here.</ResponseText></Response><Response><QuestionID>CoverLetter</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739255</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739256</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739257</QuestionID><ResponseText>222-222-2222</ResponseText></Response></Responses></RequestApplication>"
-                status = Cb.application.submit_app(request_xml)
-                status.should == true
-            end
-
-            it 'should get incomplete status message from api' do
-                request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID>#{Cb.configuration.job_did}</JobDID></RequestApplication>"
-                status = Cb.application.submit_app(request_xml)
-                status.should == false
-            end
-        end
+          @did = job.did
+          job.did.length.should >= 19
+      end
     end
+
+    context '.submit_app' do
+      it 'should send application info to api', :vcr => {:cassette_name => 'job/application/submit_app'} do
+          request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID>#{Cb.configuration.job_did}</JobDID><HostSite>WR</HostSite><SiteID>cbnsv</SiteID><Responses><Response><QuestionID>ApplicantName</QuestionID><ResponseText>captain morgain</ResponseText></Response><Response><QuestionID>ApplicantEmail</QuestionID><ResponseText>captainmorgan@careerbuilder.com</ResponseText></Response><Response><QuestionID>Resume</QuestionID><ResponseText>Resume goes here.</ResponseText></Response><Response><QuestionID>CoverLetter</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739255</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739256</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739257</QuestionID><ResponseText>222-222-2222</ResponseText></Response></Responses></RequestApplication>"
+          status = Cb.application.submit_app(request_xml)
+          status.should == true
+      end
+
+      it 'should get incomplete status message from api' do
+          request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID>#{Cb.configuration.job_did}</JobDID></RequestApplication>"
+          status = Cb.application.submit_app(request_xml)
+          status.should == false
+      end
+    end
+  end
 end
