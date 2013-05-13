2e8c398c380ce76c6f652904640ef1b3e5ac836b
Jesse Retchko <JesseRetchko@gmail.com>
2013-05-13 16:24:32 -0400

    Lots of changes around application. Still not working 100%, but in better shape. Stay tuned!

--- spec/cb/cb_application_api_spec.rb
+++ spec/cb/cb_application_api_spec.rb
@@ -9,7 +9,7 @@ module Cb
           job = search[Random.new.rand(0..24)]
 
           result = Cb.application.for_job job.did
-          result.is_a?(Cb::CbApp).should == true
+          result.is_a?(Cb::CbApplicationSchema).should == true
 
           @did = job.did
           job.did.length.should >= 19
@@ -18,13 +18,13 @@ module Cb
 
     context '.submit_app' do
       it 'should send application info to api', :vcr => {:cassette_name => 'job/application/submit_app'} do
-          request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID>#{Cb.configuration.job_did}</JobDID><HostSite>WR</HostSite><SiteID>cbnsv</SiteID><Responses><Response><QuestionID>ApplicantName</QuestionID><ResponseText>captain morgain</ResponseText></Response><Response><QuestionID>ApplicantEmail</QuestionID><ResponseText>captainmorgan@careerbuilder.com</ResponseText></Response><Response><QuestionID>Resume</QuestionID><ResponseText>Resume goes here.</ResponseText></Response><Response><QuestionID>CoverLetter</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739255</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739256</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739257</QuestionID><ResponseText>222-222-2222</ResponseText></Response></Responses></RequestApplication>"
+          request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID></JobDID><HostSite>WR</HostSite><SiteID>cbnsv</SiteID><Responses><Response><QuestionID>ApplicantName</QuestionID><ResponseText>captain morgain</ResponseText></Response><Response><QuestionID>ApplicantEmail</QuestionID><ResponseText>captainmorgan@careerbuilder.com</ResponseText></Response><Response><QuestionID>Resume</QuestionID><ResponseText>Resume goes here.</ResponseText></Response><Response><QuestionID>CoverLetter</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739255</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739256</QuestionID><ResponseText></ResponseText></Response><Response><QuestionID>6739257</QuestionID><ResponseText>222-222-2222</ResponseText></Response></Responses></RequestApplication>"
           status = Cb.application.submit_app(request_xml)
           status.should == true
       end
 
       it 'should get incomplete status message from api' do
-          request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID>#{Cb.configuration.job_did}</JobDID></RequestApplication>"
+          request_xml  = "<RequestApplication><DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey><Test>true</Test><JobDID></JobDID></RequestApplication>"
           status = Cb.application.submit_app(request_xml)
           status.should == false
       end
