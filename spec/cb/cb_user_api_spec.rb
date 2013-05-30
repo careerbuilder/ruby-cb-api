ef6ec8f42588e30c1d0a2bcee7909fb9a5ba75ac
kyle.bumpus@careerbuilder.com <kyle.bumpus@careerbuilder.com>
2013-05-30 12:57:22 -0400

    changed style of rspec assertions to match new convention

--- spec/cb/cb_user_api_spec.rb
+++ spec/cb/cb_user_api_spec.rb
@@ -7,63 +7,63 @@ module Cb
 			it 'should change a user password', :vcr => { :cassette_name => 'user/change_password/success' } do
 				result = Cb.user.change_password 'XRHL3TC5VWHV37S9J55S', 'BikeTire3', 'BikeTire3', true
 
-				result.cb_response.errors.nil?.should == true
-				result.cb_response.status.should == 'Success (Test)'
-				result.should == true
+        expect(result.cb_response.errors.nil?).to be true
+        expect(result.cb_response.status).to be == 'Success (Test)'
+        expect(result).to be true
 			end
 
 			it 'should fail without a new password', :vcr => { :cassette_name => 'user/change_password/no_new_password' } do
 				result = Cb.user.change_password 'XRHL3TC5VWHV37S9J55S', 'BikeTire4', ''
 
-				result.cb_response.errors.count.should == 1
-				result.cb_response.errors[0].should == 'NewPassword requires a value'
-				result.cb_response.status.should == 'Fail'
-				result.should == false
+        expect(result.cb_response.errors.count).to be == 1
+        expect(result.cb_response.errors[0]).to be == 'NewPassword requires a value'
+        expect(result.cb_response.status).to be == 'Fail'
+        expect(result).to be false
 			end
 
 			it 'should fail without an old password', :vcr => { :cassette_name => 'user/change_password/no_old_password' } do
 				result = Cb.user.change_password 'XRHL3TC5VWHV37S9J55S', '', 'BikeTire4'
 
-				result.cb_response.errors.count.should == 1
-				result.cb_response.errors[0].should == 'OldPassword requires a value'
-				result.cb_response.status.should == 'Fail'
-				result.should == false
+        expect(result.cb_response.errors.count).to be == 1
+        expect(result.cb_response.errors[0]).to be == 'OldPassword requires a value'
+        expect(result.cb_response.status).to be == 'Fail'
+        expect(result).to be false
 			end
 
 			it 'should fail without an external id', :vcr => { :cassette_name => 'user/change_password/no_external_id' } do
 				result = Cb.user.change_password '', 'BikeTire3', 'BikeTire4'
 
-				result.cb_response.errors.count.should == 1
-				result.cb_response.errors[0].should == 'ExternalID requires a value'
-				result.cb_response.status.should == 'Fail'
-				result.should == false
+        expect(result.cb_response.errors.count).to be == 1
+        expect(result.cb_response.errors[0]).to be == 'ExternalID requires a value'
+        expect(result.cb_response.status).to be == 'Fail'
+        expect(result).to be false
 			end
 
 			it 'should fail with incorrect old password', :vcr => { :cassette_name => 'user/change_password/incorrect_old_password' } do
 				result = Cb.user.change_password 'XRHL3TC5VWHV37S9J55S', 'incorrect_password', 'BikeTire4'
 
-				result.cb_response.errors.count.should == 1
-				result.cb_response.errors[0].should == 'ExternalID and password do not match.'
-				result.cb_response.status.should == 'Fail'
-				result.should == false
+        expect(result.cb_response.errors.count).to be == 1
+        expect(result.cb_response.errors[0]).to be == 'ExternalID and password do not match.'
+        expect(result.cb_response.status).to be == 'Fail'
+        expect(result).to be false
 			end
 
 			it 'should fail with bad credentials', :vcr => { :cassette_name => 'user/change_password/bad_credentials' } do
 				result = Cb.user.change_password 'bad_external_id', 'bad_password', 'BikeTire4'
 
-				result.cb_response.errors.count.should == 1
-				result.cb_response.errors[0].should == 'No owned user exists for given ExternalID.'
-				result.cb_response.status.should == 'Fail'
-				result.should == false
+        expect(result.cb_response.errors.count).to be == 1
+        expect(result.cb_response.errors[0]).to be == 'No owned user exists for given ExternalID.'
+        expect(result.cb_response.status).to be == 'Fail'
+        expect(result).to be false
 			end
 
 			it 'should fail with an insecure new password', :vcr => { :cassette_name => 'user/change_password/insecure_new_password' } do
 				result = Cb.user.change_password 'XRHL3TC5VWHV37S9J55S', 'BikeTire3', 'bad_password'
 
-				result.cb_response.errors.count.should == 1
-				result.cb_response.errors[0].should == 'Your password must be between 8 - 15 characters and must contain at least three of the following: upper case letter, lower case letter, number, symbol.'
-				result.cb_response.status.should == 'Fail'
-				result.should == false
+        expect(result.cb_response.errors.count).to be == 1
+        expect(result.cb_response.errors[0]).to be == 'Your password must be between 8 - 15 characters and must contain at least three of the following: upper case letter, lower case letter, number, symbol.'
+        expect(result.cb_response.status).to be == 'Fail'
+        expect(result).to be false
 			end
 		end
 
@@ -73,14 +73,14 @@ module Cb
 
 				xml_wrapper = Nokogiri::XML.parse(xml).elements
 
-				xml_wrapper.count.should == 1 # Assert one root node.
-				xml_wrapper[0].name.should == 'Request' # Assert root node name.
-				
-				xml.include?('<ExternalID>a</ExternalID>').should == true
-				xml.include?('<OldPassword>b</OldPassword>').should == true
-				xml.include?('<NewPassword>c</NewPassword>').should == true
-				xml.include?('<Test>true</Test>').should == true
-				xml.include?("<DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey>").should == true
+        expect(xml_wrapper.count).to be == 1 # Assert one root node.
+        expect(xml_wrapper[0].name).to be == 'Request' # Assert root node name.
+
+        expect(xml.include?('<ExternalID>a</ExternalID>')).to be true
+        expect(xml.include?('<OldPassword>b</OldPassword>')).to be true
+        expect(xml.include?('<NewPassword>c</NewPassword>')).to be true
+        expect(xml.include?('<Test>true</Test>')).to be true
+        expect(xml.include?("<DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey>")).to be true
 			end
     end
 
@@ -91,25 +91,25 @@ module Cb
       it 'should delete a user', :vcr => { :cassette_name => 'user/delete/success' } do
         result = Cb.user.delete external_id, password, true
 
-        result.cb_response.errors.nil?.should == true
-        result.cb_response.status.should == 'Success (Test)'
-        result.should == true
+        expect(result.cb_response.errors.nil?).to be true
+        expect(result.cb_response.status).to be == 'Success (Test)'
+        expect(result).to be true
       end
 
       it 'should fail with no password', :vcr => { :cassette_name => 'user/delete/no_password' } do
         result = Cb.user.delete external_id, nil, true
 
-        result.cb_response.errors.nil?.should == false
-        result.cb_response.status.should == 'Fail (Test)'
-        result.should == false
+        expect(result.cb_response.errors.nil?).to be false
+        expect(result.cb_response.status).to be == 'Fail (Test)'
+        expect(result).to be false
       end
 
       it 'should fail with no external_id', :vcr => { :cassette_name => 'user/delete/no_external_id' } do
         result = Cb.user.delete nil, password, true
 
-        result.cb_response.errors.nil?.should == false
-        result.cb_response.status.should == 'Fail (Test)'
-        result.should == false
+        expect(result.cb_response.errors.nil?).to be false
+        expect(result.cb_response.status).to be == 'Fail (Test)'
+        expect(result).to be false
       end
     end
 
@@ -119,13 +119,13 @@ module Cb
 
         xml_wrapper = Nokogiri::XML.parse(xml).elements
 
-        xml_wrapper.count.should == 1 # Assert one root node.
-        xml_wrapper[0].name.should == 'Request' # Assert root node name.
+        expect(xml_wrapper.count).to be == 1 # Assert one root node.
+        expect(xml_wrapper[0].name).to be == 'Request' # Assert root node name.
 
-        xml.include?('<ExternalID>a</ExternalID>').should == true
-        xml.include?('<Password>b</Password>').should == true
-        xml.include?('<Test>true</Test>').should == true
-        xml.include?("<DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey>").should == true
+        expect(xml.include?('<ExternalID>a</ExternalID>')).to be true
+        expect(xml.include?('<Password>b</Password>')).to be true
+        expect(xml.include?('<Test>true</Test>')).to be true
+        expect(xml.include?("<DeveloperKey>#{Cb.configuration.dev_key}</DeveloperKey>")).to be true
       end
     end
 	end
