6a699abd418424ddd704273fab254e94361423e7
David Posey <rangemonk@gmail.com>
2013-06-03 14:42:19 -0400

    User retrieve API.

--- lib/cb/clients/user_api.rb
+++ lib/cb/clients/user_api.rb
@@ -3,7 +3,27 @@ require 'nokogiri'
 
 module Cb
   class UserApi
-	#############################################################
+    #############################################################
+    ## Retrieve a user
+    ##
+    ## For detailed information around this API please visit:
+    ## http://www.careerbuilder.com/api/UserInfo.aspx
+    #############################################################
+    def self.retrieve external_id, password, test_mode = false
+      my_api = Cb::Utils::Api.new
+
+      cb_response = my_api.cb_post Cb.configuration.uri_user_retrieve, :body => build_retrieve_request(external_id, password, test_mode)
+
+      json_hash = JSON.parse cb_response.response.body
+
+      user = CbUser.new json_hash['ResponseUserInfo']['UserInfo']
+
+      my_api.append_api_responses user, json_hash['ResponseUserInfo']
+
+      return user
+    end
+
+    #############################################################
     ## Change a user's password
     ##
     ## For detailed information around this API please visit:
@@ -12,20 +32,26 @@ module Cb
     def self.change_password external_id, old_password, new_password, test_mode = false
     	result = false
 
-        my_api = Cb::Utils::Api.new
-        cb_response = my_api.cb_post Cb.configuration.uri_user_change_password, :body => build_change_password_request(external_id, old_password, new_password, test_mode)
-        json_hash = JSON.parse cb_response.response.body
+      my_api = Cb::Utils::Api.new
+      cb_response = my_api.cb_post Cb.configuration.uri_user_change_password, :body => build_change_password_request(external_id, old_password, new_password, test_mode)
+      json_hash = JSON.parse cb_response.response.body
 
-        my_api.append_api_responses result, json_hash['ResponseUserChangePW']
+      my_api.append_api_responses result, json_hash['ResponseUserChangePW']
 
-        if result.cb_response.status.include? 'Success'
-        	result = true
-        	my_api.append_api_responses result, json_hash['ResponseUserChangePW']
-        end
+      if result.cb_response.status.include? 'Success'
+      	result = true
+      	my_api.append_api_responses result, json_hash['ResponseUserChangePW']
+      end
 
-        result
+      result
     end
 
+    #############################################################
+    ## De-activate a user
+    ##
+    ## For detailed information around this API please visit:
+    ## http://www.careerbuilder.com/api/UserInfo.aspx
+    #############################################################
     def self.delete external_id, password, test_mode = false
       result = false
 
@@ -44,15 +70,28 @@ module Cb
     end
 
     private
+    def self.build_retrieve_request external_id, password, test_mode
+      builder = Nokogiri::XML::Builder.new do
+        Request {
+          ExternalID_ external_id
+          Password_ password
+          Test_ test_mode.to_s
+          DeveloperKey_ Cb.configuration.dev_key
+        }
+      end
+
+      builder.to_xml
+    end
+
     def self.build_change_password_request external_id, old_password, new_password, test_mode
 	    builder = Nokogiri::XML::Builder.new do
-			Request {
-				ExternalID_ external_id
-				OldPassword_ old_password
-				NewPassword_ new_password
-				Test_ test_mode.to_s
-				DeveloperKey_ Cb.configuration.dev_key
-			}
+  			Request {
+  				ExternalID_ external_id
+  				OldPassword_ old_password
+  				NewPassword_ new_password
+  				Test_ test_mode.to_s
+  				DeveloperKey_ Cb.configuration.dev_key
+  			}
 		end
 		
 		builder.to_xml
