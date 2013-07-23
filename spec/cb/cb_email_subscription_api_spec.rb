effdbbd34c0e831a4b6a8205ceea92ab7d6c2708
Justin Roberts <justin.roberts@careerbuilder.com>
2013-07-23 15:58:46 -0400

    adding some error handling to email subscription

--- spec/cb/cb_email_subscription_api_spec.rb
+++ spec/cb/cb_email_subscription_api_spec.rb
@@ -16,13 +16,56 @@ module Cb
             false
         )
 
-        test_subscription.career_resources.should == subscription.career_resources
-        test_subscription.product_sponsor_info.should == subscription.product_sponsor_info
-        test_subscription.applicant_survey_invites.should == subscription.applicant_survey_invites
-        test_subscription.job_recs.should == subscription.job_recs
+        test_subscription.career_resources.should == subscription.career_resources.to_s
+        test_subscription.product_sponsor_info.should == subscription.product_sponsor_info.to_s
+        test_subscription.applicant_survey_invites.should == subscription.applicant_survey_invites.to_s
+        test_subscription.job_recs.should == subscription.job_recs.to_s
+
+
+      end
+      it 'retrieve a users email subscription and unsubscribe to all' do
+        did = 'XRHS6FP78P5H30WLMB4G'
+        subscription = Cb.email_subscription.retrieve_by_did(did)
+
+        test_subscription = Cb.email_subscription.modify_subscription(
+            did,
+            subscription.career_resources,
+            subscription.product_sponsor_info,
+            subscription.applicant_survey_invites,
+            subscription.job_recs,
+            true
+        )
+
+        test_subscription.career_resources.should == 'false'
+        test_subscription.product_sponsor_info.should == 'false'
+        test_subscription.applicant_survey_invites.should == 'false'
+        test_subscription.job_recs.should == 'false'
+
+
+      end
+      it 'retrieve a users email subscription and update it' do
+        did = 'XRHS6FP78P5H30WLMB4G'
+        subscription = Cb.email_subscription.retrieve_by_did(did)
+        subscription.applicant_survey_invites = true
+        subscription.career_resources = true
+
+        test_subscription = Cb.email_subscription.modify_subscription(
+            did,
+            subscription.career_resources,
+            subscription.product_sponsor_info,
+            subscription.applicant_survey_invites,
+            subscription.job_recs,
+            'false'
+        )
+
+        test_subscription.career_resources.should == subscription.career_resources.to_s
+        test_subscription.product_sponsor_info.should == subscription.product_sponsor_info.to_s
+        test_subscription.applicant_survey_invites.should == subscription.applicant_survey_invites.to_s
+        test_subscription.job_recs.should == subscription.job_recs.to_s
 
 
       end
     end
+
   end
 end
