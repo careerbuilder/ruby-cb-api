2dbdbe0d0230a7d2bda2982cd3d8f3d47ddea753
Justin Roberts <justin.roberts@careerbuilder.com>
2013-07-23 10:14:09 -0400

    adding email_subscription api calls

--- lib/cb/config.rb
+++ lib/cb/config.rb
@@ -10,7 +10,8 @@ module Cb
                   :uri_application_external,
                   :uri_application_registered, :uri_user_change_password,
                   :uri_user_delete, :uri_user_retrieve,
-                  :uri_job_branding, :uri_saved_job_search_create
+                  :uri_job_branding, :uri_saved_job_search_create,
+                  :uri_subscription_retrieve, :uri_subscription_modify
 
     def initialize
       Cb::Utils::Country.inject_convenience_methods
@@ -34,7 +35,7 @@ module Cb
       @uri_user_change_password           ||= '/v2/User/ChangePW'
       @uri_user_delete                    ||= '/v2/User/delete'
       @uri_user_retrieve                  ||= '/v2/user/retrieve'
-      @uri_job_branding                   ||= '/branding'=
+      @uri_job_branding                   ||= '/branding'
       @uri_subscription_retrieve          ||= '/v1/user/subscription/retrieve'
       @uri_subscription_modify            ||= '/v1/user/subscription'
       @uri_saved_job_search_create        ||= '/v2/savedsearch/create'
