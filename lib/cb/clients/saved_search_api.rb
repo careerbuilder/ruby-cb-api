16d989f8f773d2a9cea9c32ffc649afc68b7c363
Jon Molinaro <Jon.Molinaro@Careerbuilder.com>
2013-07-29 13:54:43 -0400

    Saved Search Adjustments

--- lib/cb/clients/saved_search_api.rb
+++ lib/cb/clients/saved_search_api.rb
@@ -12,13 +12,9 @@ module Cb
     #############################################################
     def self.create params
       my_api = Cb::Utils::Api.new
-
       cb_response = my_api.cb_post Cb.configuration.uri_saved_search_create, :body => CbSavedSearch.new(params).to_xml
-
       json_hash = JSON.parse cb_response.response.body
-
       saved_search = CbSavedSearch.new json_hash['SavedJobSearch']
-
       my_api.append_api_responses saved_search, json_hash['SavedJobSearch']
 
       return saved_search
@@ -31,15 +27,37 @@ module Cb
     ## http://api.careerbuilder.com/savedsearchinfo.aspx
     #############################################################
     def self.update params
-      result = false
-
       my_api = Cb::Utils::Api.new
       cb_response = my_api.cb_post Cb.configuration.uri_saved_search_update, :body => CbSavedSearch.new(params).to_xml
       json_hash = JSON.parse cb_response.response.body
-
       saved_search = CbSavedSearch.new json_hash['SavedJobSearch']
-
       my_api.append_api_responses saved_search, json_hash['SavedJobSearch']
+
+      return saved_search
+    end
+
+    #############################################################
+    ## Delete a Saved Search
+    ## (Returns Boolean: Cb::SavedSearchApi.delete? => true/false)
+    ##
+    ## For detailed information around this API please visit:
+    ## http://api.careerbuilder.com/savedsearchinfo.aspx
+    #############################################################
+
+    def self.delete params #developer_key, external_user_id, external_id, host_site
+      result = false
+
+      my_api = Cb::Utils::Api.new
+      #cb_response = my_api.cb_post Cb.configuration.uri_saved_search_delete, :query => {:developerkey=> developer_key, :externaluserid=> external_user_id, :externalid=> external_id, :hostsite=> host_site}
+      cb_response = my_api.cb_post Cb.configuration.uri_saved_search_delete, CbSavedSearch.new(params).to_xml
+      json_hash = JSON.parse cb_response.response.body
+
+      my_api.append_api_responses result, json_hash['SavedJobSearch']
+
+      if result.cb_response.status.include? 'Success'
+        result = true
+        my_api.append_api_responses result, json_hash['SavedJobSearch']
+      end
 
       result
     end
@@ -52,18 +70,16 @@ module Cb
     ## found from running a SavedSearchApi.all call. This is a
     ## 64 character long ID.
     ##
+    ## HostSite (From Saved Search List) Required*
+    ##
     ## For detailed information around this API please visit:
     ## http://api.careerbuilder.com/savedsearchinfo.aspx
     #############################################################
-    def self.retrieve developer_key, external_user_id, external_id
+    def self.retrieve developer_key, external_user_id, external_id, host_site
       my_api = Cb::Utils::Api.new
-
-      cb_response = my_api.cb_get Cb.configuration.uri_saved_search_retrieve, :query => {:developerkey=> developer_key, :externaluserid=> external_user_id, :externalid=> external_id}
-
+      cb_response = my_api.cb_get Cb.configuration.uri_saved_search_retrieve, :query => {:developerkey=> developer_key, :externaluserid=> external_user_id, :externalid=> external_id, :hostsite=> host_site}
       json_hash = JSON.parse cb_response.response.body
-
       saved_search = CbSavedSearch.new json_hash['SavedJobSearch']
-
       my_api.append_api_responses saved_search, json_hash['SavedJobSearch']
 
       return saved_search
@@ -75,17 +91,11 @@ module Cb
     ## For detailed information around this API please visit:
     ## http://api.careerbuilder.com/savedsearchinfo.aspx
     #############################################################
-    def self.list developer_key=Cb.configuration.dev_key, external_user_id
+    def self.list developer_key, external_user_id
       my_api = Cb::Utils::Api.new
-
-      Cb.configuration.dev_key = developer_key
-
       cb_response = my_api.cb_get Cb.configuration.uri_saved_search_list, :query => {:developerkey=>developer_key, :ExternalUserId=>external_user_id}
-
       json_hash = JSON.parse cb_response.response.body
-
       saved_search = CbSavedSearch.new json_hash['SavedJobSearches']
-
       my_api.append_api_responses saved_search, json_hash['SavedJobSearches']
 
       return saved_search
