dbcf21bb362a5aff6ed135c15553e6c4a92ae831
Matt Moldavan <matt.moldavan@careerbuilder.com>
2013-08-09 11:07:50 -0400

    .

---
+++ spec/cb/cb_resume_api_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+module Cb
+  describe Cb::ResumeApi do
+    context '.own_all' do
+      #Cb.resume.retrieve
+      #resumes = Cb.resume.own_all('XRHP5HT66R55L6RVP6R9', true)
+
+      #expect(resumes.cb_response.errors.nil?).to be true
+      #expect(resumes.nil?)
+    end
+  end
+end
