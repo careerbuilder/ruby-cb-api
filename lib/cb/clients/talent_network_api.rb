6a51d970a7316103554939afd0b694af826cd9ce
Raul Gil <rgil@lat-rgil.local>
2013-08-16 10:32:23 -0400

    TN Join Questions and JobInfo API calls done. tests and some refactoring to be done as well as other calls to be completed.

--- lib/cb/clients/talent_network_api.rb
+++ lib/cb/clients/talent_network_api.rb
@@ -5,28 +5,47 @@ module Cb
 		## http://api.careerbuilder.com/TalentNetwork.aspx
 		####################################################
 
-		def self.join_form_questions(tndid, responsetype="xml")
+		def self.join_form_questions(tndid)
 			## Load the join form questions for a TalentNetworkDID
-		
 			my_api = Cb::Utils::Api.new()
-			cb_response = my_api.cb_get("#{Cb.configuration.uri_tn_join_questions}/#{tndid}/#{responsetype}", :query=>{:DeveloperKey=>Cb.configuration.dev_key})
-			p cb_response
+			cb_response = my_api.cb_get("#{Cb.configuration.uri_tn_join_questions}/#{tndid}/json")
+			json_hash = JSON.parse(cb_response.response.body)
+
+			tn_questions_collection = TalentNetwork.new(json_hash)
+			my_api.append_api_responses(tn_questions_collection, json_hash)
+
+			return tn_questions_collection
 		end
 
-		def self.join_form_branding(tndid, responsetype="json")
+		def self.join_form_branding(tndid)
 			## Gets branding information (stylesheets, copytext, etc...) for the join form.
+			my_api = Cb::Utils::Api.new
+			cb_response = my_api.cb_get("#{Cb.configuration.uri_tn_join_form_branding}/#{tndid}")
+			json_hash = JSON.parse(cb_response.response.body)
 		end
 
 		def self.join_form_geography(tnlanguage="USEnglish")
 			## Gets locations needed to fill the Geography question from the Join Form Question API
+			my_api = Cb::Utils::Api.new
+			cb_response = my_api.cb_get("#{Cb.configuration.uri_tn_join_form_geo}", :query=>{:TNLanguage=>"#{tnlanguage}"})
+			json_hash = JSON.parse(cb_response.response.body)
 		end
 
 		def self.member_create
 			## Creates a member based on the form built with the Join Form Questions API call
 		end
 
-		def self.tn_job_information(job_did, responsetype="json")
+		def self.tn_job_information(job_did, join_form_intercept="false")
+			my_api = Cb::Utils::Api.new
+			cb_response = my_api.cb_get("#{Cb.configuration.uri_tn_job_info}/#{job_did}/json", :query=> {
+				 :RequestJoinFormIntercept=>join_form_intercept})
+			json_hash = JSON.parse(cb_response.response.body)
 			
+			if json_hash.has_key?('Response')
+				tn_job_info = TalentNetwork::JobInfo.new(json_hash['Response'])
+				my_api.append_api_responses(tn_job_info, json_hash)
+				return tn_job_info
+			end
 		end
 	end #TalentNetworkJoinQuestions
 end #module
