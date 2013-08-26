1e4de7f7f78d962e41c7242c6aed23b1e4f6b14b
Christina Chatham <christina.chatham@careerbuilder.com>
2013-08-26 16:57:49 -0400

    Added attaching default json hash to object

--- lib/cb/clients/user_api.rb
+++ lib/cb/clients/user_api.rb
@@ -11,9 +11,9 @@ module Cb
     #############################################################
     def self.retrieve external_id, test_mode = false
       my_api = Cb::Utils::Api.new
-      json_hash = my_api.cb_post Cb.configuration.uri_user_retrieve, :body => build_retrieve_request(external_id, test_mode)
 
       json_hash = my_api.cb_post Cb.configuration.uri_user_retrieve, :body => build_retrieve_request(external_id, true)
+
       if json_hash.has_key? 'ResponseUserInfo'
         if json_hash['ResponseUserInfo'].has_key? 'UserInfo'
           user = CbUser.new json_hash['ResponseUserInfo']['UserInfo']
@@ -46,6 +46,8 @@ module Cb
         end
       end
 
+      my_api.append_api_responses result, json_hash
+
       result
     end
 
@@ -69,6 +71,8 @@ module Cb
           my_api.append_api_responses result, json_hash['ResponseUserDelete']
         end
       end
+
+      my_api.append_api_responses result, json_hash
 
       result
     end
@@ -95,9 +99,9 @@ module Cb
           Test_ test_mode.to_s
           DeveloperKey_ Cb.configuration.dev_key
         }
-    end
-    
-    builder.to_xml
+      end
+
+      builder.to_xml
     end
 
     def self.build_delete_request external_id, password, test_mode
