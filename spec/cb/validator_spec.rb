05db6b44f561726f350160821a866848a23f44d4
Christina Chatham <christina.chatham@careerbuilder.com>
2013-08-27 15:52:55 -0400

    Added testing for 404 status with html returning from api

--- spec/cb/validator_spec.rb
+++ spec/cb/validator_spec.rb
@@ -22,36 +22,43 @@ module Cb
       end
 
 
-      it 'should return empty hash when body is empty', :vcr => { :cassette_name => 'user/retrieve/success' } do
+      it 'should return empty hash when body is empty', :vcr => { :cassette_name => 'validator/normal_request' } do
          response = HTTParty.post url, :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
          response.response.body = ''
          validation = ResponseValidator.validate(response)
          validation.blank?.should be_true
       end
 
-      it 'should return empty hash when body is improper JSON', :vcr => { :cassette_name => 'user/retrieve/success' } do
+      it 'should return empty hash when body is improper json/xml', :vcr => { :cassette_name => 'validator/normal_request' } do
         response = HTTParty.post url, :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
         response.response.body = 'Json'
         validation = ResponseValidator.validate(response)
         validation.blank?.should be_true
       end
 
-      it 'should return empty hash when body is improper XML', :vcr => { :cassette_name => 'user/retrieve/success' } do
-        response = HTTParty.post url, :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
-        response.response.body = 'xml'
-        validation = ResponseValidator.validate(response)
-        validation.blank?.should be_true
-      end
-
-      it 'should return empty hash when response is nil', :vcr => { :cassette_name => 'user/retrieve/success' } do
+      it 'should return empty hash when response is nil', :vcr => { :cassette_name => 'validator/normal_request' } do
         response = HTTParty.post url, :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
         response = nil
         validation = ResponseValidator.validate(response)
         validation.blank?.should be_true
       end
 
-      it 'should return empty hash when url is invalid', :vcr => { :cassette_name => 'user/retrieve/success' } do
+      it 'should return empty hash when url is invalid', :vcr => { :cassette_name => 'validator/bad_url_request' } do
         response = HTTParty.post url[0...(url.length-1)], :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
+        validation = ResponseValidator.validate(response)
+        validation.blank?.should be_true
+      end
+
+      it 'should return a full json hash when response status is not 200 and content is json', :vcr => { :cassette_name => 'validator/bad_url_request'} do
+        response = HTTParty.post url[0...(url.length-1)], :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
+        response.response.body = '{"TestJson":{"Test":"True"}}'
+        validation = ResponseValidator.validate(response)
+        validation.blank?.should be_false
+      end
+
+      it 'should return a blank json hash when status is not 200 and content is html', :vcr => { :cassette_name => 'validator/bad_url_request' } do
+        response = HTTParty.post url[0...(url.length-1)], :body => build_retrieve_request('XRHP5HT66R55L6RVP6R9', true)
+        response.response.body = '<!DOCTYPE html></html>'
         validation = ResponseValidator.validate(response)
         validation.blank?.should be_true
       end
