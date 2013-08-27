650b26b6b4c153cbecc59bd5c8dda55ff026beea
kyle.bumpus@careerbuilder.com <kyle.bumpus@careerbuilder.com>
2013-08-27 16:41:58 -0400

    fixing api calls to make sure we're populating the cb_response.errors method properly

--- lib/cb/clients/category_api.rb
+++ lib/cb/clients/category_api.rb
@@ -7,10 +7,16 @@ module Cb
       json_hash = my_api.cb_get(Cb.configuration.uri_job_category_search)
 
       categoryList = []
-      if json_hash.has_key?('ResponseCategories') && json_hash['ResponseCategories'].has_key?('Categories')
-        json_hash["ResponseCategories"]["Categories"]["Category"].each do |cat|
-          categoryList << CbCategory.new(cat)
+
+      if json_hash.has_key?('ResponseCategories')
+
+        if json_hash['ResponseCategories'].has_key?('Categories')
+          json_hash['ResponseCategories']['Categories']['Category'].each do |cat|
+            categoryList << CbCategory.new(cat)
+          end
         end
+
+        my_api.append_api_responses(categoryList, json_hash['ResponseCategories'])
       end
 
       my_api.append_api_responses(categoryList, json_hash)
@@ -23,14 +29,18 @@ module Cb
       json_hash = my_api.cb_get(Cb.configuration.uri_job_category_search, :query => {:CountryCode => host_site})
 
       categoryList = []
-      if json_hash.has_key?('ResponseCategories') && json_hash['ResponseCategories'].has_key?('Categories')
-        if json_hash['ResponseCategories']['Categories']['Category'].is_a?(Array)
-          json_hash['ResponseCategories']['Categories']['Category'].each do |cat|
-            categoryList << CbCategory.new(cat)
+      if json_hash.has_key?('ResponseCategories')
+        if json_hash['ResponseCategories'].has_key?('Categories')
+          if json_hash['ResponseCategories']['Categories']['Category'].is_a?(Array)
+            json_hash['ResponseCategories']['Categories']['Category'].each do |cat|
+              categoryList << CbCategory.new(cat)
+            end
+          elsif json_hash['ResponseCategories']['Categories']['Category'].is_a?(Hash) && json_hash.length < 2
+            categoryList << CbCategory.new(json_hash['ResponseCategories']['Categories']['Category'])
           end
-        elsif json_hash['ResponseCategories']['Categories']['Category'].is_a?(Hash) && json_hash.length < 2
-          categoryList << CbCategory.new(json_hash['ResponseCategories']['Categories']['Category'])
         end
+
+        my_api.append_api_responses(categoryList, json_hash['ResponseCategories'])
       end
 
       my_api.append_api_responses(categoryList, json_hash)
