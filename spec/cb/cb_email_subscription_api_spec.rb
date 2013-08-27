31b333618aeae8cc948a4aaf68f914d90ea9676a
Christina Chatham <christina.chatham@careerbuilder.com>
2013-08-27 13:45:45 -0400

    Added cb_response meta programming to email_subscription

--- spec/cb/cb_email_subscription_api_spec.rb
+++ spec/cb/cb_email_subscription_api_spec.rb
@@ -7,6 +7,11 @@ module Cb
         did = 'XRHS6FP78P5H30WLMB4G'
         subscription = Cb.email_subscription.retrieve_by_did(did, 'WR')
 
+        subscription.nil?.should be_false
+        subscription.cb_response.errors.empty?.should be_true
+        subscription.cb_response.status.should == 'Success'
+        subscription.api_error.should == false
+
         test_subscription = Cb.email_subscription.modify_subscription(
             did,
             'WR',
@@ -17,16 +22,26 @@ module Cb
             false
         )
 
+        test_subscription.nil?.should be_false
         test_subscription.career_resources.should == subscription.career_resources.to_s
         test_subscription.product_sponsor_info.should == subscription.product_sponsor_info.to_s
         test_subscription.applicant_survey_invites.should == subscription.applicant_survey_invites.to_s
         test_subscription.job_recs.should == subscription.job_recs.to_s
 
+        test_subscription.cb_response.errors.empty?.should be_true
+        test_subscription.cb_response.status.should == 'Success'
+        test_subscription.api_error.should == false
 
       end
+
       it 'retrieve a users email subscription and unsubscribe to all' do
         did = 'XRHS6FP78P5H30WLMB4G'
         subscription = Cb.email_subscription.retrieve_by_did(did, 'WR')
+
+        subscription.nil?.should be_false
+        subscription.cb_response.errors.empty?.should be_true
+        subscription.cb_response.status.should == 'Success'
+        subscription.api_error.should == false
 
         test_subscription = Cb.email_subscription.modify_subscription(
             did,
@@ -38,6 +53,11 @@ module Cb
             true
         )
 
+        test_subscription.nil?.should be_false
+        test_subscription.cb_response.errors.empty?.should be_true
+        test_subscription.cb_response.status.should == 'Success'
+        test_subscription.api_error.should == false
+
         test_subscription.career_resources.should == 'false'
         test_subscription.product_sponsor_info.should == 'false'
         test_subscription.applicant_survey_invites.should == 'false'
@@ -48,6 +68,12 @@ module Cb
       it 'retrieve a users email subscription and update it' do
         did = 'XRHS6FP78P5H30WLMB4G'
         subscription = Cb.email_subscription.retrieve_by_did(did, 'WR')
+
+        subscription.nil?.should be_false
+        subscription.cb_response.errors.empty?.should be_true
+        subscription.cb_response.status.should == 'Success'
+        subscription.api_error.should == false
+
         subscription.applicant_survey_invites = true
         subscription.career_resources = true
 
@@ -61,11 +87,45 @@ module Cb
             'false'
         )
 
+        test_subscription.nil?.should be_false
+        test_subscription.cb_response.errors.empty?.should be_true
+        test_subscription.cb_response.status.should == 'Success'
+        test_subscription.api_error.should == false
+
         test_subscription.career_resources.should == subscription.career_resources.to_s
         test_subscription.product_sponsor_info.should == subscription.product_sponsor_info.to_s
         test_subscription.applicant_survey_invites.should == subscription.applicant_survey_invites.to_s
         test_subscription.job_recs.should == subscription.job_recs.to_s
+      end
 
+      it 'should set errors and return nil if bogus did' do
+        did = 'bogus'
+        subscription = Cb.email_subscription.retrieve_by_did(did, 'WR')
+
+        subscription.nil?.should be_true
+        subscription.cb_response.errors.empty?.should be_false
+        subscription.api_error.should == false
+
+      end
+
+      it 'should set errors with empty did' do
+        did = ''
+        subscription = Cb.email_subscription.retrieve_by_did(did, 'WR')
+
+        subscription.nil?.should be_true
+        subscription.cb_response.errors.empty?.should be_false
+        subscription.api_error.should == false
+
+      end
+
+      it 'should set api error on bogus request' do
+        correct_url = Cb.configuration.uri_subscription_retrieve
+
+        Cb.configuration.uri_subscription_retrieve = Cb.configuration.uri_subscription_retrieve + 'a'
+        subscription = Cb.email_subscription.retrieve_by_did('XRHS6FP78P5H30WLMB4G', 'WR')
+        Cb.configuration.uri_subscription_retrieve = correct_url
+
+        subscription.api_error.should == true
 
       end
     end
