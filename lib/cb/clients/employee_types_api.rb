4d4f0b4ade792dde129ff0c12736a31f75548875
kyle.bumpus@careerbuilder.com <kyle.bumpus@careerbuilder.com>
2013-08-27 16:41:58 -0400

    fixing api calls to make sure we're populating the cb_response.errors method properly

--- lib/cb/clients/employee_types_api.rb
+++ lib/cb/clients/employee_types_api.rb
@@ -7,32 +7,39 @@ module Cb
 			my_api = Cb::Utils::Api.new()
 			json_hash = my_api.cb_get(Cb.configuration.uri_employee_types)
 			
-			return array_of_objects_from_hash(json_hash)
+			return array_of_objects_from_hash(json_hash, my_api)
 		end
 
 		def self.search_by_host_site(hostsite)
 			my_api = Cb::Utils::Api.new()
 			json_hash = my_api.cb_get(Cb.configuration.uri_employee_types, :query => {:CountryCode => hostsite})
 
-			return array_of_objects_from_hash(json_hash)
+			return array_of_objects_from_hash(json_hash, my_api)
 		end
 
 		private
-		def self.array_of_objects_from_hash(json_hash)
+		def self.array_of_objects_from_hash(json_hash, my_api)
 
 			employee_types = []
-			if json_hash.has_key?('ResponseEmployeeTypes') &&
-         json_hash['ResponseEmployeeTypes'].has_key?('EmployeeTypes') &&
-         json_hash['ResponseEmployeeTypes']['EmployeeTypes'].present?
+			if json_hash.has_key?('ResponseEmployeeTypes')
+        if json_hash['ResponseEmployeeTypes'].has_key?('EmployeeTypes') &&
+            json_hash['ResponseEmployeeTypes']['EmployeeTypes'].present?
 
-				if json_hash["ResponseEmployeeTypes"]["EmployeeTypes"]["EmployeeType"].is_a?(Array)
-					json_hash["ResponseEmployeeTypes"]["EmployeeTypes"]["EmployeeType"].each do |et|
-						employee_types << CbEmployeeType.new(et)
-					end
-				elsif json_hash["ResponseEmployeeTypes"]["EmployeeTypes"]["EmployeeType"].is_a?(Hash) && json_hash.length < 2
-					employee_types << CbEmployeeType.new(json_hash["ResponseEmployeeTypes"]["EmployeeTypes"]["EmployeeType"])
-				end
-			end
+          if json_hash['ResponseEmployeeTypes']['EmployeeTypes']['EmployeeType'].is_a?(Array)
+
+            json_hash['ResponseEmployeeTypes']['EmployeeTypes']['EmployeeType'].each do |et|
+              employee_types << CbEmployeeType.new(et)
+            end
+
+          elsif json_hash['ResponseEmployeeTypes']['EmployeeTypes']['EmployeeType'].is_a?(Hash) && json_hash.length < 2
+            employee_types << CbEmployeeType.new(json_hash['ResponseEmployeeTypes']['EmployeeTypes']['EmployeeType'])
+          end
+
+        end
+        my_api.append_api_responses(employee_types, json_hash['ResponseEmployeeTypes'])
+      end
+
+      my_api.append_api_responses(employee_types, json_hash)
 			
 			return employee_types
 		end
