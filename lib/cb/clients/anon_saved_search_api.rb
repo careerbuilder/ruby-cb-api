b048d2b1fca7a9b7559159d3d13e6110f0c26527
kyle.bumpus@careerbuilder.com <kyle.bumpus@careerbuilder.com>
2013-09-05 13:22:51 -0400

    added additional tests for anon_saved_search_api call and added api_error functionality

--- lib/cb/clients/anon_saved_search_api.rb
+++ lib/cb/clients/anon_saved_search_api.rb
@@ -4,34 +4,38 @@ module Cb
       args = args[0] if args.is_a?(Array) && args.count == 1
       my_api = Cb::Utils::Api.new
 
-      cb_response = my_api.cb_post Cb.configuration.uri_anon_saved_search_create, :body => CbSavedSearch.new(args).create_to_xml
+      json_hash = my_api.cb_post Cb.configuration.uri_anon_saved_search_create, :body => CbSavedSearch.new(args).create_to_xml
 
-      json_hash = JSON.parse(cb_response.response.body)
-      json_hash['AnonymousSavedSearch']['ExternalID'] = json_hash['ExternalID']
+      if json_hash.has_key? 'AnonymousSavedSearch'
+        json_hash['AnonymousSavedSearch']['ExternalID'] = json_hash['ExternalID']
+      end
 
-      if json_hash['Errors'].size < 1
+      if json_hash.has_key?('Errors') && json_hash['Errors'].size < 1
         saved_search = CbSavedSearch.new(json_hash['AnonymousSavedSearch'])
       else
         saved_search = CbSavedSearch.new(json_hash)
       end
 
-      return saved_search
+      my_api.append_api_responses(saved_search, json_hash)
 
+      return saved_search
     end
 
     def self.delete *args
       args = args[0] if args.is_a?(Array) && args.count == 1
       my_api = Cb::Utils::Api.new
 
-      cb_response = my_api.cb_post Cb.configuration.uri_anon_saved_search_delete, :body => CbSavedSearch.new(args).delete_anon_to_xml
+      json_hash = my_api.cb_post Cb.configuration.uri_anon_saved_search_delete, :body => CbSavedSearch.new(args).delete_anon_to_xml
 
-      json_hash = JSON.parse(cb_response.response.body)
-
-      if json_hash['Errors'].size < 1
-        return json_hash['Status']
+      if json_hash.has_key?('Errors') && json_hash['Errors'].size < 1
+        response = json_hash['Status']
       else
-        return json_hash['Errors']
+        response = json_hash['Errors']
       end
+
+      my_api.append_api_responses(response, json_hash)
+
+      return response
     end
   end
 end
