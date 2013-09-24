52a765357f5cf31b253f3a31de98317555544534
tkling <tyling@gmail.com>
2013-09-24 17:05:06 -0400

    huzzah, a test that passes!

--- spec/cb/cb_spot_api_spec.rb
+++ spec/cb/cb_spot_api_spec.rb
@@ -1,18 +1,25 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 module Cb
   describe Cb::SpotApi do
 
     describe '#retrieve' do
-      it 'raises an exception when things' do
-
+      before :each do
+        @criteria = Cb::SpotRetrieveCriteria.new
+        @criteria.maxitems      = 5
+        @criteria.language      = 'WMEnglish'
+        @criteria.sortdirection = 'Descending'
+        @criteria.sortfield     = 'StartDT'
+        @criteria.contenttype   = 'ArticleMgt:WMArticles2'
       end
 
+      it 'returns an array of Cb::Spot models', :vcr => { :cassette_name => 'spot/retrieve' } do
+        models = Cb::SpotApi.retrieve @criteria
 
-
+        models.should be_a Array
+        models.first.should be_a Cb::Spot
+      end
     end
-
-
 
   end
 end
