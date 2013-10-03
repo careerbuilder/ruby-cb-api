942f5fe178ccc11533598a372c32f4136194d641
Kyle Heddon <kyle.heddon@careerbuilder.com>
2013-10-03 10:31:29 -0400

    updated cb_job_spec to include a describe block for initialize

--- spec/cb/models/cb_job_spec.rb
+++ spec/cb/models/cb_job_spec.rb
@@ -2,20 +2,24 @@ require 'spec_helper'
 
 module Cb
   describe CbJob do
-    context 'When ApplyRequirements hash is contained in the hash passed into the constructor' do
-      let(:job_hash) {{
-        'ApplyRequirements' => {
-          'ApplyRequirement' => 'noonlineapply'
-        }
-      }}
-      it 'then the apply_requirements field should be set' do
-        job = Cb::CbJob.new job_hash
-        job.apply_requirements.should == ['noonlineapply']
+    describe '#initialize' do
+
+      context 'When ApplyRequirements hash is contained in the hash passed into the constructor' do
+        let(:job_hash) {{
+          'ApplyRequirements' => {
+            'ApplyRequirement' => 'noonlineapply'
+          }
+        }}
+        it 'then the apply_requirements field should be set' do
+          job = Cb::CbJob.new job_hash
+          job.apply_requirements.should == ['noonlineapply']
+        end
+        it 'and the ResponseArrayExtractor should have received the message, #extract' do
+          Cb::Utils::ResponseArrayExtractor.should_receive(:extract).with(job_hash, 'ApplyRequirements')
+          Cb::CbJob.new job_hash
+        end
       end
-      it 'and the ResponseArrayExtractor should have received the message, #extract' do
-        Cb::Utils::ResponseArrayExtractor.should_receive(:extract).with(job_hash, 'ApplyRequirements')
-        Cb::CbJob.new job_hash
-      end
+      
     end
   end
 end
