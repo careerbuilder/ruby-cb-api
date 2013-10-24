ecff14b84d86ba20b35b7658078bf30d4f731ee6
Justin Roberts <justin.roberts@careerbuilder.com>
2013-10-24 11:42:02 -0400

    adding test for set_base_uri

--- spec/cb/config_spec.rb
+++ spec/cb/config_spec.rb
@@ -16,5 +16,18 @@ module Cb
         config.base_uri.should_not be_nil
       end
     end
+    context 'uri' do
+      it 'should change base_uri' do
+        uri = 'hello world'
+        config = Cb.configuration
+        default_uri = config.base_uri
+
+        config.set_base_uri(uri)
+        config.base_uri.should == uri
+
+        config.set_base_uri(default_uri)
+        config.base_uri.should == default_uri
+      end
+    end
   end
 end
