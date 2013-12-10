455322bc467c04d9ff1cf893ff5336cbb7556b51
tkling <tyling@gmail.com>
2013-12-10 15:21:22 -0500

    formatting!

--- lib/cb/models/implementations/employee_type.rb
+++ lib/cb/models/implementations/employee_type.rb
@@ -5,9 +5,9 @@ module Cb
       attr_accessor	:code, :name, :language
 
       def initialize(args={})
-        @code			||= args['Code'] || ''
-        @name			||= args['Name']["#text"] || ''
-        @language	||= args['Name']["@language"] || ''
+        @code			= args['Code'] || ''
+        @name			= args['Name']["#text"] || ''
+        @language	= args['Name']["@language"] || ''
       end
     end
   end
