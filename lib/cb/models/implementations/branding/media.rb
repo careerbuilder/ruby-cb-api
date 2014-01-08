55bc2b155e2450416b951d74fef2b4ebf65f52de
Jonathan <jonbarlo@gmail.com>
2014-01-08 15:28:14 -0600

    Job branding add video to media attribute
    job branding did not had any video reference (attribute of
    job_branding.media object)…now this was added in order to be used in
    job branding premium feature

--- lib/cb/models/implementations/branding/media.rb
+++ lib/cb/models/implementations/branding/media.rb
@@ -3,7 +3,7 @@ module Cb
 
     class Media
 
-      attr_accessor :header, :header_type, :tablet_banner, :mobile_logo, :footer, :is_multi_video
+      attr_accessor :header, :header_type, :tablet_banner, :mobile_logo, :footer, :video, :is_multi_video
 
       def initialize args = {}
         @header = args['Header'] || ''
@@ -11,6 +11,7 @@ module Cb
         @tablet_banner = args['TabletBanner'] || ''
         @mobile_logo = args['MobileLogo'] || ''
         @footer = args['Footer'] || ''
+        @video = args['Video'] || ''
         @is_multi_video = args['IsMultiVideo'] == 'true'
       end
 
