089a30e6f24991ce16bcd5d1dfc2dcdbe77dd6df
tkling <tyling@gmail.com>
2014-03-07 09:51:12 -0500

    moving predicate testing to its own top level context, adding not-in-response tests, some cleaning up'

--- spec/cb/models/implementations/job_spec.rb
+++ spec/cb/models/implementations/job_spec.rb
@@ -19,41 +19,54 @@ module Cb::Models
           Job.new job_hash
         end
       end
+    end
 
-      # if another 'boolean' field is added to the model with a matching predicate to go with, add it
-      # to the set of mappings below and it will test itself for you!
-      context 'predicates:' do
-        response_to_model_mappings = [
-          { :api_field => 'HasQuestionnaire',    :predicate_method => :has_questionnaire? },
-          { :api_field => 'CanBeQuickApplied',   :predicate_method => :can_be_quick_applied? },
-          { :api_field => 'ManagesOthers',       :predicate_method => :manages_others? },
-          { :api_field => 'IsScreenerApply',     :predicate_method => :screener_apply? },
-          { :api_field => 'IsSharedJob',         :predicate_method => :shared_job? },
-          { :api_field => 'RelocationCovered',   :predicate_method => :relocation_covered? },
-          { :api_field => 'ExternalApplication', :predicate_method => :external_application? },
-        ]
+    # if another 'boolean' field is added to the model with a matching predicate to go with, add it
+    # to the set of mappings below and it will test itself for you!
+    context 'predicate methods:' do
+      response_to_model_mappings = [
+        { :api_field => 'HasQuestionnaire',    :predicate_method => :has_questionnaire? },
+        { :api_field => 'CanBeQuickApplied',   :predicate_method => :can_be_quick_applied? },
+        { :api_field => 'ManagesOthers',       :predicate_method => :manages_others? },
+        { :api_field => 'IsScreenerApply',     :predicate_method => :screener_apply? },
+        { :api_field => 'IsSharedJob',         :predicate_method => :shared_job? },
+        { :api_field => 'RelocationCovered',   :predicate_method => :relocation_covered? },
+        { :api_field => 'ExternalApplication', :predicate_method => :external_application? },
+      ]
 
-        response_to_model_mappings.each do |mapping|
-          context mapping[:api_field] do
-            context 'when in response and set to (string) ' do
-              context 'True' do
-                it 'should respond true' do
-                  job_hash = { mapping[:api_field] => 'True' }
-                  expect(Job.new(job_hash).send(mapping[:predicate_method])).to be_true
-                end
+      response_to_model_mappings.each do |mapping|
+        response_field_name = mapping[:api_field]
+        predicate_method = mapping[:predicate_method]
+
+        context "#{response_field_name}/#{predicate_method}" do          
+          context 'when in response and set to (string) ' do
+            context 'True' do
+              it 'should respond true' do
+                job_hash = { response_field_name => 'True' }
+                job = Job.new(job_hash)
+                expect(job.send(predicate_method)).to be_true
               end
+            end
 
-              context 'False' do
-                it 'should response false' do
-                  job_hash = { mapping[:api_field] => 'False' }
-                  expect(Job.new(job_hash).send(mapping[:predicate_method])).to be_false
-                end
+            context 'False' do
+              it 'should response false' do
+                job_hash = { response_field_name => 'False' }
+                job = Job.new(job_hash)
+                expect(job.send(predicate_method)).to be_false
               end
             end
           end
-        end
 
+          context 'when not in response' do
+            it 'returns false' do
+              job_hash = Hash.new
+              job = Job.new(job_hash)
+              expect(job.send(predicate_method)).to be_false
+            end
+          end
+        end
       end
+
     end
   end
 end
