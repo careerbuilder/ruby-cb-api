bae97b39a2c9d44414e8467d6da06409f714ce3c
Kyle Heddon <kyle.heddon@careerbuilder.com>
2014-03-11 13:59:17 -0400

    shelve this for now

--- spec/cb/models/implementations/saved_search_spec.rb
+++ spec/cb/models/implementations/saved_search_spec.rb
@@ -61,5 +61,58 @@ module Cb
       end
     end
 
+    describe '#update_to_xml' do
+
+      let(:search_parameters) { Models::SavedSearch::SearchParameters.new }
+      before {
+        if false == true
+          saved_search.host_site = 'US'
+          saved_search.cobrand = 'AOLer'
+          saved_search.search_name = 'Yolo'
+          add_search_params
+          saved_search.search_parameters = search_parameters
+          saved_search.is_daily_email = true
+          Cb.configuration.dev_key = 'who dat'
+        end
+      }
+      it 'serialized correctly' do
+        pending 'not ready yet'
+
+        xml = saved_search.delete_anon_to_xml
+        expect(xml).to eq <<-eos
+          <Request>
+            <ExternalID>BigMoom</ExternalID>
+            <DeveloperKey>who dat</DeveloperKey>
+            <Test>false</Test>
+          </Request>
+        eos
+      end
+
+      def add_search_params
+        search_parameters.boolean_operator = 'AND'
+        search_parameters.category = 'medical'
+        search_parameters.job_category = 'medical jobs'
+        search_parameters.education_code = 'EDU#1'
+        search_parameters.emp_type = 'FullTime'
+        search_parameters.exclude_company_names = 'CB'
+        search_parameters.exclude_job_titles = 'janitor'
+        search_parameters.exclude_keywords = 'moom'
+        search_parameters.country = 'Merica'
+        search_parameters.industry_codes = 'IN123'
+        search_parameters.job_title = 'nursing person'
+        search_parameters.keywords = 'nurse'
+        search_parameters.location = 'ATL'
+        search_parameters.order_by = 'date'
+        search_parameters.order_direction = 'up'
+        search_parameters.pay_high = 100
+        search_parameters.pay_low = 2
+        search_parameters.posted_within = 8
+        search_parameters.radius = 13
+        search_parameters.specific_education = true
+        search_parameters.exclude_national = true
+        search_parameters.pay_info_only = true
+      end
+    end
+
   end
 end
