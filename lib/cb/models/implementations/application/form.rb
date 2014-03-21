4b2369eb036e7104e3d21289f4afca9d11c72f3b
tkling <tyling@gmail.com>
2014-03-21 09:13:14 -0400

    rest of the model properties

--- lib/cb/models/implementations/application/form.rb
+++ lib/cb/models/implementations/application/form.rb
@@ -3,19 +3,31 @@ module Cb
     class Application
 
       class Form < ApiResponseModel
-        attr_reader :job_did, :job_title, :is_shared_apply, :question_list
+        attr_reader :job_did, :job_title, :is_shared_apply, :question_list, :requirements,
+                    :degree_required, :travel_required, :experience_required, :external_application,
+                    :total_questions, :total_required_questions
 
         protected
 
         def required_fields
-          %w(JobDID JobTitle IsSharedApply QuestionList)
+          %w(
+            JobDID JobTitle IsSharedApply QuestionList Requirements DegreeRequired TravelRequired
+            ExperienceRequired ExternalApplication TotalQuestions TotalRequiredQuestions
+          )
         end
 
         def set_model_properties
-          @job_did         = api_response['JobDID']
-          @job_title       = api_response['JobTitle']
-          @is_shared_apply = api_response['IsSharedApply']
-          @question_list   = api_response['QuestionList'].map { |question_hash| Question.new(question_hash) }
+          @job_did                  = api_response['JobDID']
+          @job_title                = api_response['JobTitle']
+          @is_shared_apply          = api_response['IsSharedApply']
+          @requirements             = api_response['Requirements']
+          @degree_required          = api_response['DegreeRequired']
+          @travel_required          = api_response['TravelRequired']
+          @total_questions          = api_response['TotalQuestions'].to_i
+          @total_required_questions = api_response['TotalRequiredQuestions'].to_i
+          @experience_required      = api_response['ExperienceRequired']
+          @external_application     = api_response['ExternalApplication']
+          @question_list            = api_response['QuestionList'].map { |question_hash| Question.new(question_hash) }
         end
       end
 
