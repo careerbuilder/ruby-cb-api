70ee25f454899751ad57b5637a506c35909bb9d6
tkling <tyling@gmail.com>
2014-03-21 09:04:58 -0400

    application form - client method, url, response, models

---
+++ lib/cb/models/implementations/application/form.rb
@@ -0,0 +1,59 @@
+module Cb
+  module Models
+    class Application
+
+      class Form < ApiResponseModel
+        attr_reader :job_did, :job_title, :is_shared_apply, :question_list
+
+        protected
+
+        def required_fields
+          %w(JobDID JobTitle IsSharedApply QuestionList)
+        end
+
+        def set_model_properties
+          @job_did         = api_response['JobDID']
+          @job_title       = api_response['JobTitle']
+          @is_shared_apply = api_response['IsSharedApply']
+          @question_list   = api_response['QuestionList'].map { |question_hash| Question.new(question_hash) }
+        end
+      end
+
+      class Question < ApiResponseModel
+        attr_reader :expected_response_format, :is_required, :question_id, :question_text, :question_type, :answers
+
+        protected
+
+        def required_fields
+          %w(ExpectedResponseFormat IsRequired QuestionID QuestionText QuestionType Answers)
+        end
+
+        def set_model_properties
+          @is_required   = api_response['IsRequired']
+          @question_id   = api_response['QuestionID']
+          @question_text = api_response['QuestionText']
+          @question_type = api_response['QuestionType']
+          @answers       = api_response['Answers'].map { |answer_hash| Answer.new(answer_hash) }
+          @expected_response_format = api_response['ExpectedResponseFormat']
+        end
+      end
+
+      class Answer < ApiResponseModel
+        attr_reader :answer_id, :answer_text, :question_id
+
+        protected
+
+        def required_fields
+          %w(AnswerID AnswerText QuestionID)
+        end
+
+        def set_model_properties
+          @answer_id   = api_response['AnswerID']
+          @answer_text = api_response['AnswerText']
+          @question_id = api_response['QuestionID']
+        end
+      end
+
+    end
+  end
+end
