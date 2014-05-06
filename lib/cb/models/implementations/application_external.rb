ca63cc1155202d9e20a7dc50b26b75b36de55de9
Chris Little <razenghan@gmail.com>
2014-05-06 16:09:39 -0400

    Substring on constructor instead, test update

--- lib/cb/models/implementations/application_external.rb
+++ lib/cb/models/implementations/application_external.rb
@@ -1,13 +1,15 @@
 module Cb
   module Models
     class ApplicationExternal
+
+      IPATH_LENGTH = 10
       attr_accessor :job_did, :email, :site_id, :ipath, :apply_url, :is_external_link_apply
 
       def initialize(args = {})
         @job_did                = args[:job_did] || ''
         @email                  = args[:email] || ''
         @site_id                = args[:site_id] || 'cbnsv'
-        @ipath                  = args[:ipath] || ''
+        @ipath                  = args[:ipath].slice(0,IPATH_LENGTH) || ''
         @is_external_link_apply = args[:is_external_link_apply] || false
         @apply_url              = ''
       end
@@ -18,7 +20,7 @@ module Cb
         ret += "<EmailAddress>#{@email}</EmailAddress>"
         ret += "<JobDID>#{@job_did}</JobDID>"
         ret += "<SiteID>#{@site_id}</SiteID>"
-        ret += "<IPath>#{@ipath[0..9]}</IPath>"
+        ret += "<IPath>#{@ipath}</IPath>"
         ret += "<IsExternalLinkApply>#{@is_external_link_apply}</IsExternalLinkApply>"
         ret += "<HostSite>#{Cb.configuration.host_site}</HostSite>"
         ret += "</Request>"
--- spec/cb/models/implementations/application_external_spec.rb
+++ spec/cb/models/implementations/application_external_spec.rb
@@ -7,7 +7,7 @@ module Cb::Models
       @job_did = 'J1234567890'
       @email = 'dontspammebro@whoa.net'
       @site_id = 'xxx'
-      @ipath = 'shiv'
+      @ipath = 'shivitandshivitgood'
       @is_external_link_apply = true
       @application = ApplicationExternal.new(
         { job_did: @job_did,
@@ -22,7 +22,7 @@ module Cb::Models
       it 'should create an empty new external application' do
         @application.job_did.should == @job_did
         @application.email.should == @email
-        @application.ipath.should == @ipath
+        @application.ipath.should == @ipath.slice(0,10)
         @application.is_external_link_apply.should == @is_external_link_apply
         @application.site_id.should == @site_id
       end
