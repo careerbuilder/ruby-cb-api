15703a25eb1450b04bb16a057687c55d5a21dc47
Justin Roberts <justin.roberts@careerbuilder.com>
2014-05-19 16:00:54 -0400

    yield tests (only tests that a block is passed, nothing else!)

--- spec/cb/client_spec.rb
+++ spec/cb/client_spec.rb
@@ -97,5 +97,76 @@ module Cb
       end
 
     end
+    context 'api call with block' do
+
+      let(:mock_api) { double(Cb::Utils::Api) }
+
+      before :each do
+        @client = Cb::Client.new { |a| a }
+      end
+
+      context 'post' do
+        before :each do
+          mock_api.stub(:cb_post).and_return(Hash.new)
+          mock_api.stub(:append_api_responses)
+          Cb::Utils::Api.stub(:new).and_return(mock_api)
+          Cb::Utils::ResponseMap.stub(:finder).and_return(Mocks::ResponseTest)
+
+          @request = Mocks::RequestTest.new(:post)
+        end
+
+        it 'should call the api using post' do
+          mock_api.should_receive(:cb_post).
+              with('parts unknown', {:query=>nil, :headers=>nil, :body=>nil}).
+              and_yield("test").
+              and_return(Hash.new)
+
+          @client.execute(@request)
+        end
+      end
+
+      context 'get' do
+        before :each do
+          mock_api.stub(:cb_get).and_return(Hash.new)
+          mock_api.stub(:append_api_responses)
+          Cb::Utils::Api.stub(:new).and_return(mock_api)
+          Cb::Utils::ResponseMap.stub(:finder).and_return(Mocks::ResponseTest)
+
+          @request = Mocks::RequestTest.new(:get)
+        end
+
+        it 'should call the api using post' do
+          mock_api.should_receive(:cb_get).
+              with('parts unknown', {:query=>nil, :headers=>nil, :body=>nil}).
+              and_yield("test").
+              and_return(Hash.new)
+
+          @client.execute(@request)
+        end
+      end
+
+      context 'put' do
+        before :each do
+          mock_api.stub(:cb_put).and_return(Hash.new)
+          mock_api.stub(:append_api_responses)
+          Cb::Utils::Api.stub(:new).and_return(mock_api)
+          Cb::Utils::ResponseMap.stub(:finder).and_return(Mocks::ResponseTest)
+
+          @request = Mocks::RequestTest.new(:put)
+        end
+
+        it 'should call the api using post' do
+          mock_api.should_receive(:cb_put).
+              with('parts unknown', {:query=>nil, :headers=>nil, :body=>nil}).
+              and_yield("test").
+              and_return(Hash.new)
+
+          @client.execute(@request)
+        end
+      end
+
+    end
+
+
   end
 end
