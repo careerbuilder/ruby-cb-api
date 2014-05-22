2806d2755faad90a5bc522eb9890c895fcd6d6a8
Justin Roberts <justin.roberts@careerbuilder.com>
2014-05-22 13:52:37 -0400

    request, broken map test

--- spec/cb/utils/response_map_spec.rb
+++ spec/cb/utils/response_map_spec.rb
@@ -2,17 +2,25 @@
 module Cb
   describe Cb::Utils::ResponseMap do
     context '.finder' do
+
+      let(:response_map) { Cb::Utils::ResponseMap }
+
       it 'should throw an error on a bad entry' do
-        expect { Cb::Utils::ResponseMap.response_for("Hello") }.to raise_error(Cb::Utils::ResponseNotFoundError)
+        expect { response_map.response_for("Hello") }.to raise_error(Cb::Utils::ResponseNotFoundError)
+      end
+
+      it 'should test application external methods' do
+        response_map.response_for(Cb::Requests::ApplicationExternal::SubmitApplication).should == Cb::Responses::ApplicaationExternal::SubmitApplication
       end
 
       it 'should test user methods' do
-        Cb::Utils::ResponseMap.response_for(Cb::Requests::User::ChangePassword).should == Cb::Responses::User::ChangePassword
-        Cb::Utils::ResponseMap.response_for(Cb::Requests::User::CheckExisting).should == Cb::Responses::User::CheckExisting
-        Cb::Utils::ResponseMap.response_for(Cb::Requests::User::Delete).should == Cb::Responses::User::Delete
-        Cb::Utils::ResponseMap.response_for(Cb::Requests::User::Retrieve).should == Cb::Responses::User::Retrieve
-        Cb::Utils::ResponseMap.response_for(Cb::Requests::User::TemporaryPassword).should == Cb::Responses::User::TemporaryPassword
+        response_map.response_for(Cb::Requests::User::ChangePassword).should == Cb::Responses::User::ChangePassword
+        response_map.response_for(Cb::Requests::User::CheckExisting).should == Cb::Responses::User::CheckExisting
+        response_map.response_for(Cb::Requests::User::Delete).should == Cb::Responses::User::Delete
+        response_map.response_for(Cb::Requests::User::Retrieve).should == Cb::Responses::User::Retrieve
+        response_map.response_for(Cb::Requests::User::TemporaryPassword).should == Cb::Responses::User::TemporaryPassword
       end
+
 
     end
   end
