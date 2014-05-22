047e7010824cc8fcb087dd1c3876a59a331d6e7d
Justin Roberts <justin.roberts@careerbuilder.com>
2014-05-22 15:31:01 -0400

    response stuff

--- lib/cb/requests/application_external/submit_application.rb
+++ lib/cb/requests/application_external/submit_application.rb
@@ -20,11 +20,19 @@ module Cb
             <EmailAddress>#{args[:email_address]}</EmailAddress>
             <JobDID>#{args[:job_did]}</JobDID>
             <SiteID>#{args[:site_id]}</SiteID>
-            <IPath>#{args[:ipath]}</IPath>
+            <IPath>#{ipath}</IPath>
             <IsExternalLinkApply>#{args[:is_external_link_apply]}</IsExternalLinkApply>
             <HostSite>#{Cb.configuration.host_site}</HostSite>
           </Request>
           eos
+        end
+
+        private
+
+        def ipath
+          ipath_length = 10
+
+          app[:ipath].slice(0, ipath_length) || '' unless ipath.nil?
         end
 
       end
