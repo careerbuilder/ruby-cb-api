e0a1d4258ab4025dda7881e747daa23193712d5b
Justin Roberts <justin.roberts@careerbuilder.com>
2014-05-29 11:16:46 -0400

    is_a? on #ipath

--- lib/cb/requests/application_external/submit_application.rb
+++ lib/cb/requests/application_external/submit_application.rb
@@ -30,7 +30,7 @@ module Cb
         private
 
         def ipath
-          return '' unless @args[:ipath].class == String
+          return '' unless @args[:ipath].is_a?(String)
           ipath_length = 10
 
           @args[:ipath].slice(0, ipath_length)
