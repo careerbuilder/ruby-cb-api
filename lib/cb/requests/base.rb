7b60ed47f40610ee91f6e24df8cb071b687d55f6
Justin Roberts <justin.roberts@careerbuilder.com>
2014-05-29 09:51:54 -0400

    #test? change

--- lib/cb/requests/base.rb
+++ lib/cb/requests/base.rb
@@ -29,7 +29,7 @@ module Cb
       private
 
       def test?
-        (@args[:test] || Cb.configuration.test_resources || false).to_s
+        (@args[:test] || Cb.configuration.test_resources).to_s
       end
 
     end
