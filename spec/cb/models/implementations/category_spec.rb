df0548d66ce69e1a4db2e7ba9eb4367e7d654d7e
Justin Roberts <justin.roberts@careerbuilder.com>
2014-05-30 13:20:00 -0400

    more coverage!

--- spec/cb/models/implementations/category_spec.rb
+++ spec/cb/models/implementations/category_spec.rb
@@ -4,9 +4,16 @@ module Cb::Models
   describe Category do
     context ".new" do
       it 'should create a new cb category object' do
-        args = { 'Name' => { '#text' => String.new, '@language' => String.new } }
+        args = { 'Name' => { '#text' => 'name', '@language' => 'lang' },
+                 'Code' => 'code' }
         category_obj = Category.new(args)
         category_obj.is_a?(Category).should == true
+
+
+        category_obj.CategoryCode.should == 'code'
+        category_obj.CategoryLanguage.should == 'lang'
+        category_obj.CategoryName.should == 'name'
+
       end
     end
   end
