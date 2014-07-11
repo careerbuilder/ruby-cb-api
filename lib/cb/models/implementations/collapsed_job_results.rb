2ddf73fdc8a1554e5e0124867cfc1865d0e5a692
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-07-11 11:08:36 -0400

    building the collapsed job results model

---
+++ lib/cb/models/implementations/collapsed_job_results.rb
@@ -0,0 +1,26 @@
+module CB
+  module Models
+    class CollapsedJobResults
+      attr_reader :args_hash, :total_count, :last_item_index, :search_location, :grouped_jobs, :grouping_parameter
+      def initialize(args_hash)
+        @args_hash = args_hash
+        @total_count = args_hash['TotalCount']
+        @last_item_index = args_hash['LastItemIndex']
+        @search_location = args_hash['SearchMetaData']['SearchLocations']['SearchLocation'] rescue nil
+        @grouping_parameter = args_hash['GroupedJobSearchResults']['GroupingParameter']
+        @grouped_jobs = extract_collapsed_jobs(args_hash['GroupedJobSearchResults']['SearchResults'])
+      end
+
+      private
+
+      def extract_collapsed_jobs(collapsed_jobs)
+        collapsed_jobs.collect do |collapsed_job|
+          collapsed_job.collect do |job_hash|
+            Job.new(job_hash)
+          end
+        end
+      end
+
+    end
+  end
+end
