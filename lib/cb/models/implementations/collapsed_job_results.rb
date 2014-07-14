d91ff20c04a69782346f2f09c8b1791e04a54769
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-07-14 16:54:22 -0400

    adding page information

--- lib/cb/models/implementations/collapsed_job_results.rb
+++ lib/cb/models/implementations/collapsed_job_results.rb
@@ -1,14 +1,17 @@
 module Cb
   module Models
     class CollapsedJobResults
-      attr_reader :args_hash, :total_count, :last_item_index, :search_location, :grouped_jobs, :grouping_parameter
+      attr_reader :args_hash, :last_item_index, :search_location, :grouped_jobs, :grouping_parameter, :total_count, :total_pages, :first_item_index, :last_item_index
       def initialize(args_hash)
         @args_hash = args_hash
-        @total_count = args_hash['TotalCount']
         @last_item_index = args_hash['LastItemIndex']
         @search_location = args_hash['SearchMetaData']['SearchLocations']['SearchLocation'] rescue nil
         @grouping_parameter = args_hash['GroupedJobSearchResults']['GroupingParameter']
         @grouped_jobs = extract_collapsed_jobs(args_hash['GroupedJobSearchResults']['SearchResults']['JobSearchResultsGroup'])
+        @total_pages = args_hash['TotalPages']
+        @total_count = args_hash['TotalCount']
+        @first_item_index = args_hash['FirstItemIndex']
+        @last_item_index = args_hash['LastItemIndex']
       end
 
       private
