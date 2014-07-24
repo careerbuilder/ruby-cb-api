8b96610ed94fd743df34ba3f7cf66208b03d6979
David Posey <rangemonk@gmail.com>
2014-07-24 15:19:31 -0400

    tests

--- spec/cb/requests/work_status/list_spec.rb
+++ spec/cb/requests/work_status/list_spec.rb
@@ -2,51 +2,46 @@ require 'spec_helper'
 
 module Cb
   describe Cb::Requests::WorkStatus::List do
-
     describe '#new' do
       context 'without arguments' do
-        before(:each) { @request = Cb::Requests::WorkStatus::List.new({}) }
+        let(:request) { Cb::Requests::WorkStatus::List.new({}) }
 
         it 'should be correctly configured' do
-          expect(@request.endpoint_uri).to eq Cb.configuration.uri_work_status_list
-          expect(@request.http_method).to eq :get
+          expect(request.endpoint_uri).to eq Cb.configuration.uri_work_status_list
+          expect(request.http_method).to eq :get
         end
 
         it 'should have a basic query string' do
-          expect(@request.query).to eq ({ HostSite: nil })
+          expect(request.query).to eq ({ HostSite: nil })
         end
 
         it 'should have basic headers' do
-          expect(@request.headers).to eq nil
+          expect(request.headers).to eq nil
         end
 
         it 'should have a basic body' do
-          expect(@request.body).to eq nil
+          expect(request.body).to eq nil
         end
       end
 
       context 'with arguments' do
-        before :each do
-          @request = Cb::Requests::WorkStatus::List.new({
-            host_site: 'US'
-          })
-        end
+        let(:request) { Cb::Requests::WorkStatus::List.new host_site: 'US' }
 
         it 'should be correctly configured' do
-          expect(@request.endpoint_uri).to eq Cb.configuration.uri_work_status_list
-          expect(@request.http_method).to eq :get
+          expect(request.endpoint_uri).to eq Cb.configuration.uri_work_status_list
+          expect(request.http_method).to eq :get
         end
 
         it 'should have a basic query string' do
-          expect(@request.query).to eq ({ HostSite: "US" })
+          expect(request.query).to eq ({ HostSite: "US" })
         end
 
         it 'should have basic headers' do
-          expect(@request.headers).to eq nil
+          expect(request.headers).to eq nil
         end
 
         it 'should have a basic body' do
-          expect(@request.body).to eq nil
+          expect(request.body).to eq nil
         end
       end
     end
---
+++ spec/cb/responses/work_status/list_spec.rb
@@ -0,0 +1,100 @@
+require 'spec_helper'
+
+module Cb
+  describe Responses::WorkStatus::List do
+    let(:json_hash) do
+      {
+        "Errors" => nil,
+        "WorkStatuses" => [
+          {
+            "Key" => "CTCT",
+            "Description" => [
+              {
+                "Language" => "French",
+                "Value" => "Citoyen"
+              },
+              {
+                "Language" => "Dutch",
+                "Value" => "Inwoner"
+              },
+              {
+                "Language" => "English",
+                "Value" => "US Citizen"
+              }
+            ]
+          }
+        ]
+      }
+    end
+
+    describe '#new' do
+      let(:response) { Responses::WorkStatus::List.new json_hash }
+
+      it 'returns a Responses::WorkStatus::List object' do
+        expect(response).to be_a Responses::WorkStatus::List
+      end
+
+      context 'created models' do
+        let(:models) { response.models }
+
+        it 'creates Cb::Models::WorkStatus models' do
+          models.each { |model| expect(model).to be_a Models::WorkStatus }
+        end
+
+        it 'creates one model' do
+          expect(models.length).to eq 1
+        end
+
+        context 'created model' do
+          let(:model) { models[0] }
+
+          it 'assigns key to the model' do
+            expect(model.key).to eq 'CTCT'
+          end
+
+          context 'created translations' do
+            let(:translations) { model.translations }
+
+            shared_examples_for 'a translation' do
+              it 'should be a Models::WorkStatus::Translation' do
+                expect(translation).to be_a Models::WorkStatus::Translation
+              end
+
+              it 'assigns language' do
+                expect(translation.language).to eq language
+              end
+
+              it 'assigns value' do
+                expect(translation.value).to eq value
+              end
+            end
+
+            context 'first created translation' do
+              let(:translation) { translations[0] }
+              let(:language) { 'French' }
+              let(:value) { 'Citoyen' }
+
+              it_behaves_like 'a translation'
+            end
+
+            context 'first created translation' do
+              let(:translation) { translations[1] }
+              let(:language) { 'Dutch' }
+              let(:value) { 'Inwoner' }
+
+              it_behaves_like 'a translation'
+            end
+
+            context 'first created translation' do
+              let(:translation) { translations[2] }
+              let(:language) { 'English' }
+              let(:value) { 'US Citizen' }
+
+              it_behaves_like 'a translation'
+            end
+          end
+        end
+      end
+    end
+  end
+end
