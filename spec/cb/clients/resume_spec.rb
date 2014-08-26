0aeb85c50085fb1383b53436290d89a1c168e030
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-08-26 10:38:15 -0400

    Attempt at TDD. Here is the test to drive me.

---
+++ spec/cb/clients/resume_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+module Cb
+  describe Cb::Clients::Resumes do
+    context 'When a single resume is asked for with .getOneByHash' do
+      let(:content) {JSON.parse(File.read('spec/support/response_stubbs/resumeGet.json')) }
+      let(:resume_get_call) {Cb.resumes.getOneByHash(Hash.new)}
+
+      it {expect(resume_get_call).to be_a Responses::ResumeGet}
+      it {expect(resume_get_tcall.model).to be_a Cb::Models::Resume}
+      it {expect(resume_get_call.model.user_identifier).to eq('userID')}
+      it {expect(resume_get_call.model.resume_hash).to eq('hashMe')}
+    end
+  end
+end
