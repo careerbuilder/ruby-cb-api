b21216e1b51aa982e05dc4762c5203bf7694ea09
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-09-02 13:35:26 -0400

    sprout method all the things.

--- lib/cb/clients/resumes.rb
+++ lib/cb/clients/resumes.rb
@@ -4,12 +4,11 @@ module Cb
   module Clients
     class Resumes
       class << self
+        attr_reader :api_args
 
         def get_resume_by_hash(args)
-          uri = Cb.configuration.uri_resume_get.gsub(':resume_hash', args[:resume_hash])
-          query = get_query(args)
-          response = api_client.cb_get(uri, query: query)
-          Cb::Responses::Resumes::ResumeGet.new(response)
+          @api_args = args
+          Cb::Responses::Resumes::ResumeGet.new(get_resume_by_hash_response)
         end
 
         def api_client
@@ -18,10 +17,18 @@ module Cb
 
         private
 
-        def get_query(args)
+        def query
           {
-              :externalUserID => args[:externalUserID]
+              :externalUserID => @api_args[:externalUserID]
           }
+        end
+
+        def uri
+          Cb.configuration.uri_resume_get.gsub(':resume_hash', @api_args[:resume_hash])
+        end
+
+        def get_resume_by_hash_response
+          api_client.cb_get(uri, query: query)
         end
 
       end
