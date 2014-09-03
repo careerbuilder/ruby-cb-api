b75e9d332c8f3743af4e11fcc421ba0795cadc08
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-09-03 10:13:47 -0400

    making these tests more readable with sub contexts

--- spec/cb/clients/resume_spec.rb
+++ spec/cb/clients/resume_spec.rb
@@ -14,19 +14,39 @@ module Cb
 
       it {expect(resume_get_call).to be_a Responses::Resumes::ResumeGet}
       it {expect(model).to be_a Models::Resume}
-      it {expect(model.user_identifier).to eq 'userID'}
-      it {expect(model.resume_hash).to eq 'hashMe'}
-      it {expect(model.work_experience).to be_an Array}
-      it {expect(model.work_experience[0]).to be_a Models::Resumes::WorkExperience}
-      it {expect(model.salary_information).to be_a Models::Resumes::SalaryInformation}
-      it {expect(model.educations).to be_an Array}
-      it {expect(model.educations[0]).to be_a Models::Resumes::Education}
-      it {expect(model.skills_and_qualifications).to be_a Models::Resumes::SkillsAndQualifications}
-      it {expect(model.skills_and_qualifications.languages_spoken).to be_an Array}
-      it {expect(model.skills_and_qualifications.languages_spoken[0]).to be_a String}
-      it {expect(model.relocations).to be_an Array}
-      it {expect(model.relocations[0]).to be_a Models::Resumes::Relocation}
-      it {expect(model.government_and_military).to be_a Models::Resumes::GovernmentAndMilitary}
+      it {expect(model.user_identifier).to_not be_nil}
+      it {expect(model.resume_hash).to_not be_nil}
+
+      context 'and we look at the WorkExperience section' do
+        it {expect(model.work_experience).to be_an Array}
+        it {expect(model.work_experience[0]).to be_a Models::Resumes::WorkExperience}
+      end
+
+      context 'and we look at the SalaryInformation section' do
+        it {expect(model.salary_information).to be_a Models::Resumes::SalaryInformation}
+      end
+
+      context 'and we look at the Education section' do
+        it {expect(model.educations).to be_an Array}
+        it {expect(model.educations[0]).to be_a Models::Resumes::Education}
+      end
+
+      context 'and we look at the SkillsAndQualifications section' do
+        it {expect(model.skills_and_qualifications).to be_a Models::Resumes::SkillsAndQualifications}
+        context 'and even deeper in the Languages section' do
+          it {expect(model.skills_and_qualifications.languages_spoken).to be_an Array}
+          it {expect(model.skills_and_qualifications.languages_spoken[0]).to be_a String}
+        end
+      end
+
+      context 'and we look at the relocations section' do
+        it {expect(model.relocations).to be_an Array}
+        it {expect(model.relocations[0]).to be_a Models::Resumes::Relocation}
+      end
+
+      context 'and we look at the GovernmentAndMilitary section' do
+        it {expect(model.government_and_military).to be_a Models::Resumes::GovernmentAndMilitary}
+      end
     end
   end
 end
