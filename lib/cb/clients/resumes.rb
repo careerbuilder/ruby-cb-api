93b19c33b19e85086bb0745bcfc0b2289244cb5c
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-09-03 13:27:47 -0400

    removing sprout method to make more readable.

--- lib/cb/clients/resumes.rb
+++ lib/cb/clients/resumes.rb
@@ -7,15 +7,11 @@ module Cb
         attr_reader :api_args
 
         def get_resume_by_hash(args)
-          @api_args = args
-          Cb::Responses::Resumes::ResumeGet.new(get_resume_by_hash_response)
+          response = api_client.cb_get(uri, query: query)
+          Cb::Responses::Resumes::ResumeGet.new(response)
         end
 
         private
-
-        def get_resume_by_hash_response
-          api_client.cb_get(uri, query: query)
-        end
         
         def uri
           Cb.configuration.uri_resume_get.gsub(':resume_hash', @api_args[:resume_hash])
