6af6b41d1fcdbc0a6c5ae39799847afdd7c29026
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-09-22 10:39:17 -0400

    solve conflict
    Conflicts:
    	lib/cb/responses/resumes/put.rb

--- spec/cb/responses/resumes/resume_spec.rb
+++ spec/cb/responses/resumes/resume_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe Cb::Responses::Resumes::Get do
+describe Cb::Responses::Resumes::Resume do
   let(:response) { JSON.parse(File.read('spec/support/response_stubs/resume_get.json')) }
 
   context 'when the Api response comes back as expected' do
-    let(:resumes) { Cb::Responses::Resumes::Get.new(response) }
+    let(:resumes) { Cb::Responses::Resumes::Resume.new(response) }
 
     it { expect(resumes.models.first).to be_an_instance_of Cb::Models::Resume }
   end
@@ -12,7 +12,7 @@ describe Cb::Responses::Resumes::Get do
   context 'when the Api response is missing the results field' do
     it 'raises an exception' do
       response.delete('Results')
-      expect{ Cb::Responses::Resumes::Get.new(response) }.
+      expect{ Cb::Responses::Resumes::Resume.new(response) }.
         to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
           expect(ex.message).to include 'Results'
       end
