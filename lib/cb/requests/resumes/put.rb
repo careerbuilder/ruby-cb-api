daf606d25bb5f4ca11c7fbb2ea756bd7507f466f
Rex <justin.rex@careerbuilder.com>
2014-09-22 11:52:10 -0400

    more style changes

--- lib/cb/requests/resumes/put.rb
+++ lib/cb/requests/resumes/put.rb
@@ -14,24 +14,24 @@ module Cb
 
         def headers
           {
-          'DeveloperKey' => Cb.configuration.dev_key,
-          'HostSite' => Cb.configuration.host_site,
-          'Content-Type' => 'application/json'
+            'DeveloperKey' => Cb.configuration.dev_key,
+            'HostSite' => Cb.configuration.host_site,
+            'Content-Type' => 'application/json'
           }
         end
 
         def body
           {
-          userIdentifier: args[:user_identifier],
-          resumeHash: args[:resume_hash],
-          desiredJobTitle: args[:desired_job_title],
-          privacySetting: args[:privacy_setting],
-          workExperience: extract_work_experience(args[:work_experience]),
-          salaryInformation: extract_salary_information(args[:salary_information]),
-          educations: extract_educations(args[:educations]),
-          skillsAndQualifications: extract_skills_and_qualifications(args[:skills_and_qualifications]),
-          relocations: extract_relocations(args[:relocations]),
-          governmentAndMilitary: extract_government_and_military(args[:government_and_military])
+            userIdentifier: args[:user_identifier],
+            resumeHash: args[:resume_hash],
+            desiredJobTitle: args[:desired_job_title],
+            privacySetting: args[:privacy_setting],
+            workExperience: extract_work_experience(args[:work_experience]),
+            salaryInformation: extract_salary_information(args[:salary_information]),
+            educations: extract_educations(args[:educations]),
+            skillsAndQualifications: extract_skills_and_qualifications(args[:skills_and_qualifications]),
+            relocations: extract_relocations(args[:relocations]),
+            governmentAndMilitary: extract_government_and_military(args[:government_and_military])
           }.to_json
         end
 
@@ -39,12 +39,12 @@ module Cb
           if work_experience
             args[:work_experience].collect do |experience|
               {
-              jobTitle: experience[:job_title],
-              companyName: experience[:company_name],
-              employmentType: experience[:employment_type],
-              startDate: experience[:start_date],
-              endDate: experience[:end_date],
-              currentlyEmployedHere: experience[:currently_employed_here]
+                jobTitle: experience[:job_title],
+                companyName: experience[:company_name],
+                employmentType: experience[:employment_type],
+                startDate: experience[:start_date],
+                endDate: experience[:end_date],
+                currentlyEmployedHere: experience[:currently_employed_here]
               }
             end
           end
@@ -53,12 +53,12 @@ module Cb
         def extract_salary_information(salary)
           if salary
             {
-            mostRecentPayAmount: salary[:most_recent_pay_amount],
-            perHourOrPerYear: salary[:per_hour_or_per_year],
-            currencyCode: salary[:currency_code],
-            jobTitle: salary[:job_title],
-            annualBonus: salary[:annual_bonus],
-            annualCommission: salary[:annual_commission],
+              mostRecentPayAmount: salary[:most_recent_pay_amount],
+              perHourOrPerYear: salary[:per_hour_or_per_year],
+              currencyCode: salary[:currency_code],
+              jobTitle: salary[:job_title],
+              annualBonus: salary[:annual_bonus],
+              annualCommission: salary[:annual_commission],
             }
           end
         end
@@ -67,10 +67,10 @@ module Cb
           if educations
             educations.collect do |education|
               {
-              schoolName: education[:school_name],
-              majorOrProgram: education[:major],
-              degreeCode: education[:degree_code],
-              graduationDate: education[:graduation_date]
+                schoolName: education[:school_name],
+                majorOrProgram: education[:major],
+                degreeCode: education[:degree_code],
+                graduationDate: education[:graduation_date]
               }
             end
           end
@@ -79,10 +79,10 @@ module Cb
         def extract_skills_and_qualifications(skills)
           if skills
             {
-            accreditationsAndCertifications: skills[:accreditations_and_certifications],
-            languagesSpoken: skills[:languages_spoken],
-            hasManagementExperience: skills[:has_management_experience],
-            sizeOfTeamManaged: skills[:size_of_team_managed]
+              accreditationsAndCertifications: skills[:accreditations_and_certifications],
+              languagesSpoken: skills[:languages_spoken],
+              hasManagementExperience: skills[:has_management_experience],
+              sizeOfTeamManaged: skills[:size_of_team_managed]
             }
           end
         end
@@ -92,9 +92,9 @@ module Cb
           if relocations
             args[:relocations].collect do |relocate|
               {
-              city: relocate[:city],
-              adminArea: relocate[:admin_area],
-              countryCode: relocate[:country_code]
+                city: relocate[:city],
+                adminArea: relocate[:admin_area],
+                countryCode: relocate[:country_code]
               }
             end
           end
@@ -103,8 +103,8 @@ module Cb
         def extract_government_and_military(government)
           if government
             {
-            hasSecurityClearance: government[:has_security_clearance],
-            militaryExperience: government[:military_experience]
+              hasSecurityClearance: government[:has_security_clearance],
+              militaryExperience: government[:military_experience]
             }
           end
         end
