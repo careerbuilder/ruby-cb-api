61aa25a78d21b636fad38cef111914af18b76498
Rex <justin.rex@careerbuilder.com>
2014-09-30 15:26:58 -0400

    cleanup

--- spec/cb/responses/recommendations/resume_recs_spec.rb
+++ spec/cb/responses/recommendations/resume_recs_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Cb::Responses::Recommendations do
-  let(:response) { JSON.parse(File.read('spec/support/response_stubs/recommendations_for_resume.json')) }
-  let(:api_job_result_collection) { Cb::Responses::Recommendations.new(response) }
+  let(:response_hash) { JSON.parse(File.read('spec/support/response_stubs/recommendations_for_resume.json')) }
+  let(:api_job_result_collection) { Cb::Responses::Recommendations.new(response_hash) }
 
   it { expect(api_job_result_collection.models.first).to be_an_instance_of Cb::Models::RecommendedJob }
 
@@ -11,10 +11,11 @@ describe Cb::Responses::Recommendations do
   end
 
   context 'when the Api response is missing the results field' do
+    let(:response_missing_data_node) { response_hash.delete('data') }
+    let(:response) { Cb::Responses::Recommendations.new(response_missing_data_node) }
+
     it 'raises an exception' do
-      response.delete('data')
-      expect{ Cb::Responses::Recommendations.new(response) }.
-          to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
+      expect{ response }.to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
         expect(ex.message).to include 'data'
       end
     end
