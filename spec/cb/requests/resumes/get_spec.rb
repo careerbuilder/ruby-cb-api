dc345080d02e15f74d0fd12c313faae6b7bf0807
Rex <justin.rex@careerbuilder.com>
2014-09-30 15:46:19 -0400

    refactoRex

--- spec/cb/requests/resumes/get_spec.rb
+++ spec/cb/requests/resumes/get_spec.rb
@@ -2,31 +2,22 @@ require 'spec_helper'
 
 module Cb
   describe Cb::Requests::Resumes::Get do
-
-    context 'initialize without arguments' do
+    let(:headers) {
+      {
+        'DeveloperKey' => Cb.configuration.dev_key,
+        'HostSite' => Cb.configuration.host_site,
+        'Content-Type' => 'application/json'
+      }
+    }
+    describe '#new' do
       context 'without arguments' do
         let(:request) { Cb::Requests::Resumes::Get.new({}) }
 
-        it 'should be correctly configured' do
-          request.endpoint_uri.should == Cb.configuration.uri_resume_get.sub(':resume_hash', '')
-          request.http_method.should == :get
-        end
-
-        it 'should have a basic query string' do
-          request.query.should eq({ externalUserID: nil })
-        end
-
-        it 'should have basic headers' do
-          request.headers.should == {
-              'DeveloperKey' => Cb.configuration.dev_key,
-              'HostSite' => Cb.configuration.host_site,
-              'Content-Type' => 'application/json'
-          }
-        end
-
-        it 'should have a basic body' do
-          request.body.should == nil
-        end
+        it { expect(request.endpoint_uri).to eq Cb.configuration.uri_resume_get.sub(':resume_hash', '') }
+        it { expect(request.http_method).to eq :get }
+        it { expect(request.query).to eq({ externalUserID: nil }) }
+        it { expect(request.headers).to eq headers }
+        it { expect(request.body).to eq nil }
       end
 
       context 'with arguments' do
@@ -34,26 +25,11 @@ module Cb
           Cb::Requests::Resumes::Get.new({ resume_hash: 'resumeHash', external_user_id: 'externalUserId' })
         end
 
-        it 'should be correctly configured' do
-          request.endpoint_uri.should == Cb.configuration.uri_resume_get.sub(':resume_hash', 'resumeHash')
-          request.http_method.should == :get
-        end
-
-        it 'should have a basic query string' do
-          request.query.should eq ({ externalUserID: 'externalUserId' })
-        end
-
-        it 'should have basic headers' do
-          request.headers.should == {
-            'DeveloperKey' => Cb.configuration.dev_key,
-            'HostSite' => Cb.configuration.host_site,
-            'Content-Type' => 'application/json'
-          }
-        end
-
-        it 'should have a nil body' do
-          request.body.should == nil
-        end
+        it { expect(request.endpoint_uri).to eq(Cb.configuration.uri_resume_get.sub(':resume_hash', 'resumeHash')) }
+        it { expect(request.http_method).to eq :get }
+        it { expect(request.query).to eq ({ externalUserID: 'externalUserId' }) }
+        it { expect(request.headers).to eq headers }
+        it { expect(request.body).to eq nil }
       end
     end
   end
