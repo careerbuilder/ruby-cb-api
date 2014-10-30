a3b72e9b132c7cde62ecfaf35a86c6a5565a0c81
marcusmoncayo <marcus.moncayo@gmail.com>
2014-10-30 10:34:14 -0400

    added rec limit and respective test

--- spec/cb/requests/recommendations/resume_rec_spec.rb
+++ spec/cb/requests/recommendations/resume_rec_spec.rb
@@ -15,7 +15,7 @@ module Cb
 
         it { expect(request.endpoint_uri).to eq Cb.configuration.uri_recommendation_for_resume.sub(':resume_hash', '') }
         it { expect(request.http_method).to eq :get }
-        it { expect(request.query).to eq({ externalID: nil }) }
+        it { expect(request.query).to eq({ externalID: nil, countLimit: 25 }) }
         it { expect(request.body).to eq nil }
         it { expect(request.headers).to eq (headers) }
       end
@@ -27,9 +27,17 @@ module Cb
 
         it { expect(request.endpoint_uri).to eq Cb.configuration.uri_recommendation_for_resume.sub(':resume_hash', 'resumeHash') }
         it { expect(request.http_method).to eq :get }
-        it { expect(request.query).to eq ({ externalID: 'externalUserId' }) }
+        it { expect(request.query).to eq ({ externalID: 'externalUserId',:countLimit=>25 }) }
         it { expect(request.body).to eq nil }
         it { expect(request.headers).to eq (headers) }
+      end
+
+      context 'when count limit is passed as argument' do
+        let (:request) do
+          Cb::Requests::Recommendations::Resume.new({ resume_hash: 'resumeHash', external_user_id: 'externalUserId', countLimit: 50 })
+        end
+
+        it { expect(request.query).to eq ({ externalID: 'externalUserId',:countLimit=>50 }) }
       end
     end
   end
