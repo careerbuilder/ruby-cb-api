edf5b2ef84ce57198962002a06651134729deaa4
marcusmoncayo <marcus.moncayo@gmail.com>
2014-11-04 15:04:31 -0500

    refactored test

--- spec/cb/models/implementations/resume/language_code_spec.rb
+++ spec/cb/models/implementations/resume/language_code_spec.rb
@@ -2,52 +2,45 @@ require 'spec_helper'
 
 module Cb
   describe Models::Resumes::LanguageCode do
-    let(:error) do
-      begin
-        response
-      rescue ExpectedResponseFieldMissing => e
-        e.message
-      end
-    end
-
-    context 'all arguments' do
+    describe '#new' do
       let(:key_name) { 'Name' }
       let(:name) { 'name' }
       let(:key_code) { 'Code' }
       let(:code) { 'code' }
 
-      let(:args) { { key_name => name , key_code => code} }
-      let(:language_code_model) { Models::Resumes::LanguageCode.new(args) }
+      let(:error) do
+        begin
+          response
+        rescue ExpectedResponseFieldMissing => e
+          e.message
+        end
+      end
 
-      it { expect(language_code_model.name).to eq(name) }
-      it { expect(language_code_model.code).to eq(code) }
+      context 'given Name and Code' do
+        let(:args) { { key_name => name , key_code => code} }
+        let(:language_code_model) { Models::Resumes::LanguageCode.new(args) }
+
+        it { expect(language_code_model.name).to eq(name) }
+        it { expect(language_code_model.code).to eq(code) }
+      end
+
+      context 'missing both arguments' do
+        let(:response) { Models::Resumes::LanguageCode.new({}) }
+
+        it { expect(error).to eq(key_code + ' ' + key_name) }
+      end
+
+      context 'missing Code' do
+        let(:response) { Models::Resumes::LanguageCode.new({ key_name => name }) }
+
+        it { expect(error).to eq(key_code) }
+      end
+
+      context 'missing Name' do
+        let(:response) { Models::Resumes::LanguageCode.new({ key_code => code }) }
+
+        it { expect(error).to eq(key_name) }
+      end
     end
-
-    context 'no arguments' do
-      let(:error_message) { 'Code Name' }
-      let(:response) { Models::Resumes::LanguageCode.new({}) }
-
-      it { expect(error).to eq(error_message) }
-    end
-
-    context 'missing Code' do
-      let(:key) { 'Name' }
-      let(:value) { 'name' }
-      let(:error_message) { 'Code' }
-      let(:response) { Models::Resumes::LanguageCode.new({ key => value }) }
-
-      it { expect(error).to eq(error_message) }
-    end
-
-    context 'missing Name' do
-      let(:key) { 'Code' }
-      let(:value) { 'code' }
-      let(:error_message) { 'Name' }
-      let(:response) { Models::Resumes::LanguageCode.new({ key => value }) }
-
-      it { expect(error).to eq(error_message) }
-    end
-
-
   end
 end
