70f448d261395dd47f6eadec34fd38fa46fb0a96
marcusmoncayo <marcus.moncayo@gmail.com>
2014-11-12 08:04:53 -0500

    added changes after robucop

--- spec/cb/responses/data_lists/country_codes_spec.rb
+++ spec/cb/responses/data_lists/country_codes_spec.rb
@@ -3,19 +3,20 @@ require 'spec_helper'
 module Cb
   module Responses
     describe CountryCodes do
-      let(:response_stub) { MultiXml.parse(File.read('spec/support/response_stubs/country_codes.xml')) }
+      let(:response_stub) { file }
+      let(:file) { MultiXml.parse(File.read('spec/support/response_stubs/country_codes.xml')) }
       let(:response) { Cb::Responses::CountryCodes.new(response_stub) }
 
       it { expect(response.models.first).to be_an_instance_of(Cb::Models::CountryCode) }
-      it { expect(response.models.first.code).to eql('US')}
+      it { expect(response.models.first.code).to eql('US') }
 
       context 'missing node SupportedCountryCodes' do
         before do
-         response_stub['ResponseCountryCodes'].delete('SupportedCountryCodes')
+          response_stub['ResponseCountryCodes'].delete('SupportedCountryCodes')
         end
         it do
-          expect {Cb::Responses::CountryCodes.new(response_stub)}.
-              to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
+          expect { Cb::Responses::CountryCodes.new(response_stub) }
+              .to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
             expect(ex.message).to include 'SupportedCountryCodes'
           end
         end
@@ -26,8 +27,8 @@ module Cb
           response_stub['ResponseCountryCodes']['SupportedCountryCodes'].delete('CountryCode')
         end
         it do
-          expect {Cb::Responses::CountryCodes.new(response_stub)}.
-              to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
+          expect { Cb::Responses::CountryCodes.new(response_stub) }
+              .to raise_error(Cb::ExpectedResponseFieldMissing) do |ex|
             expect(ex.message).to include 'CountryCode'
           end
         end
