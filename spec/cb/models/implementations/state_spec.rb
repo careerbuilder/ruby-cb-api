81d719985fd63d1fd7779b35699bab0420dd33d3
NicholasBusby <Nicholas.Busby@CareerBuilder.com>
2014-11-19 12:28:48 -0500

    removing unnessarry line.

--- spec/cb/models/implementations/state_spec.rb
+++ spec/cb/models/implementations/state_spec.rb
@@ -4,7 +4,6 @@ module Cb
   module Models
     describe State do
       let(:state) { Models::State.new(state_hash) }
-      let(:state_hash) { JSON.parse(File.read('spec/support/response_stubs/state.json')) }
 
       context 'a state_hash is valid' do
         let(:state_hash) do
