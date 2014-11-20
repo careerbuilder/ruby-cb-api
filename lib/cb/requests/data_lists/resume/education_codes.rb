76d37e0618e9f60384ed0c2d2e14d2bbc8c3ef74
marcusmoncayo <marcus.moncayo@gmail.com>
2014-11-20 08:13:55 -0500

    modified request

--- lib/cb/requests/data_lists/resume/education_codes.rb
+++ lib/cb/requests/data_lists/resume/education_codes.rb
@@ -1,21 +1,34 @@
 module Cb
   module Requests
     module DataLists
-      class EducationCodes < Base
-        def endpoint_uri
-          Cb.configuration.uri_resume_education
+      class EducationCodes
+        attr_reader :token, :args
+
+        def initialize(token, args)
+          @token = token
+          @args = args
         end
 
-        def http_method
-          :get
+        def get
+          Cb::Responses::EducationCodes.new request.parsed
         end
 
-        def headers
-          {
-            'DeveloperKey' => Cb.configuration.dev_key,
-            'Content-Type' => 'application/json',
-            'CountryCode' => args['country_code'] ||= 'US',
-          }
+        private
+
+        def request
+          token.get(uri)
+        end
+
+        def uri
+          base_uri + query_string
+        end
+
+        def base_uri
+          Cb.configuration.base_uri + Cb.configuration.uri_education_codes
+        end
+
+        def query_string
+          args['countrycode'] ? "?countrycode=#{args['countrycode']}" : '?countrycode=US'
         end
       end
     end
