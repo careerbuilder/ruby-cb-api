3b5754aed7aa35b7e330c0832a7b07704dcbfc7f
ausmith <aaron.goldsmith@careerbuilder.com>
2014-11-24 09:43:38 -0500

    Switch to soft tabs.

--- lib/cb/models/implementations/job_report/company.rb
+++ lib/cb/models/implementations/job_report/company.rb
@@ -4,9 +4,9 @@ module Cb
       attr_accessor :company_name, :city, :state
 
       def initialize(args = {})
-        @company_name			= args['Name']  || String.new
-        @city       			= args['City']  || String.new
-        @state            = args['State'] || String.new
+        @company_name   = args['Name']  || String.new
+        @city           = args['City']  || String.new
+        @state          = args['State'] || String.new
       end
 
     end
--- lib/cb/models/implementations/job_report/degree.rb
+++ lib/cb/models/implementations/job_report/degree.rb
@@ -4,9 +4,9 @@ module Cb
       attr_accessor :degree_name, :number_of_applicants, :percentage_of_applicants
 
       def initialize(args = {})
-      	@degree_name								= args['Name'] 								|| String.new
-      	@number_of_applicants				= args['NumberOfApplicants']	|| 0.0
-      	@percentage_of_applicants		= args['PercentOfApplicants']	|| 0.0
+        @degree_name              = args['Name'] || String.new
+        @number_of_applicants     = args['NumberOfApplicants'] || 0.0
+        @percentage_of_applicants = args['PercentOfApplicants'] || 0.0
       end
 
     end
