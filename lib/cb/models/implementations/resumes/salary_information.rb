d5140790aa3e3146d20db39dfdb59da6b262ba6d
nicholasbusby <nicholas.busby@Live.com>
2015-01-19 12:47:13 -0500

    removing extra *  and fixing CodeClimate

--- lib/cb/models/implementations/resumes/salary_information.rb
+++ lib/cb/models/implementations/resumes/salary_information.rb
@@ -6,17 +6,24 @@ module Cb
                       :annual_bonus, :annual_commission, :work_experience_id
 
         def set_model_properties
-          @most_recent_pay_amount = api_response['mostRecentPayAmount']
-          @per_hour_or_per_year = api_response['PerHourOrPerYear']
-          @currency_code = api_response['CurrencyCode']
-          @annual_bonus = api_response['annualBonus']
-          @annual_commission = api_response['annualCommission']
-          @work_experience_id = api_response['workExperienceId']
+          @most_recent_pay_amount = get_api_response 'mostRecentPayAmount'
+          @per_hour_or_per_year = get_api_response 'PerHourOrPerYear'
+          @currency_code = get_api_response 'CurrencyCode'
+          @annual_bonus = get_api_response 'annualBonus'
+          @annual_commission = get_api_response 'annualCommission'
+          @work_experience_id = get_api_response 'workExperienceId'
         end
 
         def required_fields
           ['PerHourOrPerYear', 'mostRecentPayAmount']
         end
+
+        private
+
+        def get_api_response(api_key)
+          api_response['api_key']
+        end
+
       end
     end
   end
