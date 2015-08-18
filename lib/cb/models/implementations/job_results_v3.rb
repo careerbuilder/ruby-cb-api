7a61cacf446e191fee326bfe56c3e7baff25e8d2
David-Sharpe <david.sharpe@careerbuilder.com>
2015-08-18 16:12:19 -0400

    Going back to raw response.

--- lib/cb/models/implementations/job_results_v3.rb
+++ lib/cb/models/implementations/job_results_v3.rb
@@ -2,11 +2,9 @@ module Cb
   module Models
     class JobResultsV3
       attr_reader :api_response
-      attr_reader :response_metadata
       
       def initialize(response_hash = {})
-        @api_response = response_hash['data']
-        @response_metadata = response_hash['search_metadata']
+        @api_response = response_hash
       end
     end
   end
