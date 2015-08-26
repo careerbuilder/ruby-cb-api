5bed99321376e47401e7225f88bad2658a58294b
usbsnowcrash <jeffery.yeary@careerbuilder.com>
2015-08-26 17:28:00 -0400

    adding copyright to responses

--- lib/cb/responses/api_response.rb
+++ lib/cb/responses/api_response.rb
@@ -1,8 +1,17 @@
+# Copyright 2015 CareerBuilder, LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
 require 'forwardable'
 
 module Cb
   module Responses
-
     class ApiResponse
       extend Forwardable
       delegate [:timing, :errors] => :metadata
@@ -18,34 +27,36 @@ module Cb
       end
 
       protected
+
       attr_reader :metadata
 
       def extract_models
-        raise NotImplementedError.new(__method__)
+        fail NotImplementedError.new(__method__)
       end
 
       def validate_api_hash
-        raise NotImplementedError.new(__method__)
+        fail NotImplementedError.new(__method__)
       end
 
       def hash_containing_metadata
-        raise NotImplementedError.new(__method__)
+        fail NotImplementedError.new(__method__)
       end
 
       def raise_on_timing_parse_error
         true
       end
+
       def required_response_field(field_name, parent_hash)
-        raise ArgumentError.new("field_name can't be nil!")  if field_name.nil?
-        raise ArgumentError.new("parent_hash can't be nil!") if parent_hash.nil?
-        raise ExpectedResponseFieldMissing.new(field_name) unless parent_hash.has_key?(field_name)
+        fail ArgumentError.new("field_name can't be nil!")  if field_name.nil?
+        fail ArgumentError.new("parent_hash can't be nil!") if parent_hash.nil?
+        fail ExpectedResponseFieldMissing.new(field_name) unless parent_hash.key?(field_name)
       end
 
       private
 
       def set_response_variable(response_hash)
         should_raise = response_hash.nil? || response_hash.empty?
-        raise ApiResponseError.new('Response is empty!') if should_raise
+        fail ApiResponseError.new('Response is empty!') if should_raise
         @response = response_hash
       end
 
@@ -58,6 +69,5 @@ module Cb
         extract_models
       end
     end
-
   end
 end
