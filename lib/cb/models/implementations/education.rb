8b473670b9fdd9c5268b44f082a74a521cbb3e13
usbsnowcrash <jeffery.yeary@careerbuilder.com>
2015-08-27 11:30:42 -0400

    cleaning this up a bit

--- lib/cb/models/implementations/education.rb
+++ lib/cb/models/implementations/education.rb
@@ -19,8 +19,8 @@ module Cb
         @code          = args['Code'] || ''
         @language			 = args['Name']['@language'] unless args['Name'].nil?
         @text          = args['Name']['#text'] unless args['Name'].nil?
-        @language ||= ''
-        @text ||= ''
+        @language      ||= ''
+        @text          ||= ''
       end
     end
   end
