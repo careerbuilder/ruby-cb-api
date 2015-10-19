0fb62a8363d0ed6ac0c2d45c736a88c56bc9ac92
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-10-19 14:49:48 -0400

    fix each loop

--- lib/cb/clients/recommendation.rb
+++ lib/cb/clients/recommendation.rb
@@ -65,11 +65,10 @@ module Cb
       def self.for_company(company_did)
         my_api = Cb::Utils::Api.instance
         json_hash = my_api.cb_get(Cb.configuration.uri_recommendation_for_company, query: { CompanyDID: company_did })
-
         jobs = []
         if json_hash.key?('Results')
           if json_hash['Results'].key?('JobRecommendation')
-            json_hash['Results']['JobRecommendation']['Jobs'].each do |cur_job|
+            json_hash['Results']['JobRecommendation']['Jobs']['CompanyJob'].each do |cur_job|
               jobs << Models::Job.new(cur_job)
             end
             my_api.append_api_responses(jobs, json_hash['Results']['JobRecommendation'])
