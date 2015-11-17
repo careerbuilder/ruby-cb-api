a552a707a802289ce64606081499668f0cd7282d
David-Sharpe <david.sharpe@careerbuilder.com>
2015-11-17 08:52:35 -0500

    Used renamed JobResults class

--- spec/cb/models/implementations/job_results_spec.rb
+++ spec/cb/models/implementations/job_results_spec.rb
@@ -11,9 +11,9 @@
 require 'spec_helper'
 
 module Cb::Models
-  describe JobResultsV3 do
+  describe JobResults do
     let(:search_response_hash) { YAML.load open('spec/support/response_stubs/search_result_v3.yml') }
-    let(:search_results) { JobResultsV3.new(search_response_hash) }
+    let(:search_results) { JobResults.new(search_response_hash) }
 
     describe '#api_result' do
       it 'returns a Hash' do
