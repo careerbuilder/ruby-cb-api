dd548fb541b4a7dafa1ce93b9d1aba299afec009
Jeffery Yeary <jeff@debug.ninja>
2016-04-06 11:38:52 -0400

    this\?

--- spec/cb/clients/browser_id_spec.rb
+++ spec/cb/clients/browser_id_spec.rb
@@ -16,16 +16,15 @@ module Cb
 
     let(:uri) {"https://api.careerbuilder.com/consumer/browser-id?developerkey=#{ Cb.configuration.dev_key }&outputjson=true" }
     let(:data) { ['X1D1ED1AB1C2E2ACB376040B85C9CA434932CB4DB65BA1F3E7ABF98C2F08A384F471F98FEC9F8ADD9788DE91F3E6DD4E84'] }
+    let(:stub) { stub_request(:get, uri).with(headers: headers).to_return(status: 200, body: response.to_json) }
+    subject { Cb::Clients::BrowserID.get(oauth_token: 'token') }
 
+    before do
+      stub
+      subject
+    end
     context '#get' do
-      it 'performs a get and returns the browser id' do
-        stub = stub_request(:get, uri).
-            with(:headers => headers).
-            to_return(:status => 200, :body => response.to_json)
-
-        response = Cb::Clients::BrowserID.get(oauth_token: 'token')
-        expect_api_to_succeed_and_return_model(response, stub)
-      end
+      it { expect_api_to_succeed_and_return_model(subject, stub) }
     end
   end
 end
