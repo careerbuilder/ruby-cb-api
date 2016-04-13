e797d479613e6e50e4ac34668497e8f19c163e84
Jeffery Yeary <jeff@debug.ninja>
2016-04-13 12:24:55 -0400

    cutting a pre release gem

--- lib/cb/version.rb
+++ lib/cb/version.rb
@@ -9,5 +9,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 module Cb
-  VERSION = '21.0.0p1'
+  VERSION = '21.0.0.pre1'
 end
