de6457046e1654801c8bae2936282069899a0ad7
Brandon Koch <KneightSkye@gmail.com>
2016-04-27 16:45:27 -0400

    oh and added the missing spec_helper so these tests would actually run.

--- spec/cb/responses/api_response_spec.rb
+++ spec/cb/responses/api_response_spec.rb
@@ -1,3 +1,4 @@
+require 'spec_helper'
 # Copyright 2015 CareerBuilder, LLC
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
